42a647c0d011f3faa50c8088694fc055e8686cb8
farixdev <onlyfarix@gmail.com>
2026-04-13 23:47:44 +0500

    added some usefull func in utility

---
+++ lib/utils/device/device_utility.dart
@@ -0,0 +1,99 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:get/utils.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+class TDeviceUtility {
+  static void hidekeyboard(BuildContext context) {
+    FocusScope.of(context).requestFocus(FocusNode());
+  }
+
+  static Future<void> setStatusBarColor(Color color) async {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: color),
+    );
+  }
+
+  static bool isLandScapeOrientation(BuildContext context) {
+    final viewInsets = View.of(context).viewInsets;
+    return viewInsets.bottom == 0;
+  }
+
+  static bool isPortraitOrientation(BuildContext context) {
+    final viewInsets = View.of(context).viewInsets;
+    return viewInsets.bottom != 0;
+  }
+
+  static void setFullScree(bool enable) {
+    SystemChrome.setEnabledSystemUIMode(
+      enable ? SystemUiMode.immersiveSticky : SystemUiMode.edgeToEdge,
+    );
+  }
+
+  static double getScreenHeight(BuildContext context) {
+    return MediaQuery.of(Get.context!).size.width;
+  }
+
+  static double getScreenWidth(BuildContext context) {
+    return MediaQuery.of(context).size.width;
+  }
+
+  static double getPixelRatio() {
+    return MediaQuery.of(Get.context!).devicePixelRatio;
+  }
+
+  static double getStausBarHeight() {
+    return MediaQuery.of(Get.context!).padding.top;
+  }
+
+  static double getBottomNavigationBarHeight() {
+    return kBottomNavigationBarHeight;
+  }
+
+  static double getAppBarHeight() {
+    return kToolbarHeight;
+  }
+
+  static double getKeyboardHeight() {
+    final viewInsets = MediaQuery.of(Get.context!).viewInsets;
+    return viewInsets.bottom;
+  }
+
+  static Future<bool> isKeyboardVisible() async {
+    final viewInsets = MediaQuery.of(Get.context!).viewInsets;
+    return viewInsets.bottom > 0;
+  }
+
+  static void vibrate(Duration duration) {
+    HapticFeedback.vibrate();
+    Future.delayed(duration, () => HapticFeedback.vibrate());
+  }
+
+  static void hideStatuSBar() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  }
+
+  static void showStatuSBar() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  }
+
+  static Future<bool> hasinternetConnection() async{
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      return result.isNotEmpty && result[0].rawAddress.isNotEmpty;
+    } on SocketException catch (_) {
+      return false;
+    }
+  }
+
+  static void launchURL(String url) async {
+    if (await canLaunchUrlString(url)) {
+      await launchUrlString(url);
+    } else{
+      throw 'could not launch $url';
+    }
+  }
+}
