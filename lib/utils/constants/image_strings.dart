cd3b36b457c0a5fe7a0f52bd2042fa4255875cc2
farixdev <onlyfarix@gmail.com>
2026-04-14 17:15:45 +0500

    Add splash and background images for dark and light themes, and update styles for Android and iOS
    - Added splash.png and background.png for Android in drawable resources.
    - Created styles.xml for night mode and light mode in Android to define themes for splash screens.
    - Added LaunchBackground imageset with background and darkbackground images for iOS.
    - Included LaunchImage imageset with various resolutions for dark mode in iOS.

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -1,7 +1,7 @@
 class FImages {
   // app logos
-  static const String darkAppLogo = "assets/logos/t-store-splash-logo-black.png";
-  static const String larkAppLogo = "assets/logos/t-store-splash-logo-white.png";
+  static const String darkAppLogo = "assets/logos/f-store-splash-logo-black.png";
+  static const String larkAppLogo = "assets/logos/f-store-splash-logo-white.png";
 
 static const String google = "assets/logos/google-icon.png";
 static const String facebook = "assets/logos/facebook-icon.png";
