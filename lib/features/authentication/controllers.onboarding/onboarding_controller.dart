58f37d4467cfbb8bc7b1b0ec0f0160b2c60511c6
farixdev <onlyfarix@gmail.com>
2026-04-17 10:25:15 +0500

    feat: add OnBoardingController and OnBoardingScreen for onboarding flow

---
+++ lib/features/authentication/controllers.onboarding/onboarding_controller.dart
@@ -0,0 +1,22 @@
+import 'package:get/get.dart';
+
+class OnBoardingController extends GetxController {
+  static OnBoardingController get instance => Get.find();
+
+  ///variables
+  
+
+  //update current index whwen page changes
+  void updatePageIndicator(index){}
+
+  //jump to the specfic dot selected page 
+  void dotNavigationClick(index){}
+
+  //Update current index and jump to the next page
+  void nextPage(){}
+
+  //Update current index and jump to the last page
+  void skipPage(){}
+
+
+}
