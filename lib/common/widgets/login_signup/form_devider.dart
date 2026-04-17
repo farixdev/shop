a1f1f9ed7834ba806a73b45b03dfd8b1f962d50c
farixdev <onlyfarix@gmail.com>
2026-04-17 23:39:17 +0500

    feat: add FFormDevider widget for improved login/signup UI separation

---
+++ lib/common/widgets/login_signup/form_devider.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'package:shop/utils/constants/colors.dart';
+
+import 'package:shop/utils/constants/text_strings.dart';
+import 'package:shop/utils/helpers/helper_functions.dart';
+
+class FFormDevider extends StatelessWidget {
+  const FFormDevider({super.key, required this.dividerText});
+  
+  final String dividerText;
+  
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = FHelperFunctions.isDarkMode(context);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Flexible(
+          child: Divider(
+            color: dark ? FColors.darkergrey : FColors.grey,
+            thickness: 0.5,
+            indent: 60,
+            endIndent: 5,
+          ),
+        ),
+        Text(
+          FTexts.orSignInWith.capitalize!,
+          style: Theme.of(context).textTheme.labelMedium,
+        ),
+        Flexible(
+          child: Divider(
+            color: dark ? FColors.darkergrey : FColors.grey,
+            thickness: 0.5,
+            indent: 5,
+            endIndent: 60,
+          ),
+        ),
+      ],
+    );
+  }
+}
