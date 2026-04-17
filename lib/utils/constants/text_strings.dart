2d670ac958c7ddd79fd2b890ea7058b3512d5b72
farixdev <onlyfarix@gmail.com>
2026-04-17 22:28:54 +0500

    feat: add new authentication texts and update onboarding subtitles for clarity

--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -10,9 +10,43 @@ class FTexts {
   static const String onBoardingSubTitle2 = "For Seamless Transaction, Choose Your Payment Path - Your Convenience, Our Priority!";
   static const String onBoardingSubTitle3 = "From Our Doorstep to Yours - Swift, Secure adn Contactless Delivery!";
 
-  //home
-  static const String homeAppBarTitle = "Good day for shopping";
-  static const String homeAppBarSubTitle = "Faris Ahmad";
+  //authentication form texts
+  static const String firstName = "First Name";
+  static const String lastName = "Last Name";
+  static const String email = "Email";
+  static const String password = "Password";
+  static const String newPassword = "Enter New Password";
+  static const String username = "Username";
+  static const String phoneNo = "Phone Number";
+  static const String rememberMe = "Remember Me";
+  static const String forgotPassword = "Forgot Password?";
+  static const String signIn = "Sign In";
+  static const String createAccount = "Create Account";
+  static const String orSignInWith = "or sign in with";
+  static const String orSignUpWith = "or sign up with";
+  static const String iAgreeTo = "I agree to ";
+  static const String privacyPolicy = "Privacy Policy";
+  static const String termOfUse = "Term of Use";
+  static const String verificationCode = "Verification Code";
+  static const String resendEmail = "Resend Email";
+  static const String resendEmailIn = "Resend Email in";
+  
+
+  //authentication Heading Texts
+  static const String loginTitle = "Welcome Back,";
+  static const String loginSubTitle = "Discover Limitless Choices and Unmatched Convenience.";
+  static const String signupTitle = "Let's Create Your Account";
+  static const String forgotPasswordTitle = "Forgot Password?";
+  static const String forgotPasswordSubTitle = "Don't Worry! It Happens to the best of us. Please Enter The Email and We'll Send You a password reset link.";
+  static const String changeYourPasswordTitle = "Password reset email sent!";
+  static const String changeYourPasswordSubTitle = "Your Account Security is Our Priority. Please Check Your Email for the Password Reset Link and Follow the Instructions to Regain Access to Your Account.";
+  static const String confirmEmail = "Verify Your Email address";
+  static const String confirmEmailSubTitle = "Congratulations! Your Account awaits: verify your email address to unlock a world of shopping possibilities. Click the link in your inbox to confirm your email and start exploring our exclusive offers and seamless shopping experience.";
+  static const String emailNotRecievedMessage = "Didn't receive the email? cbeck your spam folder or resend the email.";
+  static const String yourAccountCreatedTitle = "Your Account Has Been Created!";
+  static const String yourAccountCreatedSubTitle = "Welcome to your ultimale shopping destination! Your account has been successfully created. unleash the joy of seamless online shopping";
+  
+
 
 
 }
