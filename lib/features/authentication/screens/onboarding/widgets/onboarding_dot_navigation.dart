8d1124bbe1fce938c5933bfb3962415cb6b5976e
farixdev <onlyfarix@gmail.com>
2026-04-17 20:45:37 +0500

    feat: update OnBoardingDotNavigation to use existing controller and improve code formatting

--- lib/features/authentication/screens/onboarding/widgets/onboarding_dot_navigation.dart
+++ lib/features/authentication/screens/onboarding/widgets/onboarding_dot_navigation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop/features/authentication/controllers/onboarding/onboarding_controller.dart';
 import 'package:shop/utils/constants/colors.dart';
 import 'package:shop/utils/constants/sizes.dart';
 import 'package:shop/utils/device/device_utility.dart';
@@ -6,23 +7,23 @@ import 'package:shop/utils/helpers/helper_functions.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnBoardingDotNavigation extends StatelessWidget {
-  const OnBoardingDotNavigation({
-    super.key,
-  });
+  const OnBoardingDotNavigation({super.key});
 
   @override
   Widget build(BuildContext context) {
-
+    final controller = OnBoardingController.instance;
     final dark = FHelperFunctions.isDarkMode(context);
+
     return Positioned(
       bottom: FDeviceUtility.getBottomNavigationBarHeight() + 25,
       left: FSizes.defaultSpace,
-    
+
       child: SmoothPageIndicator(
-        controller: PageController(),
+        controller: controller.pageController,
+        onDotClicked: controller.dotNavigationClick,
         count: 3,
         effect: ExpandingDotsEffect(
-          activeDotColor: dark? FColors.light: FColors.dark,
+          activeDotColor: dark ? FColors.light : FColors.dark,
           dotHeight: 6,
         ),
       ),
