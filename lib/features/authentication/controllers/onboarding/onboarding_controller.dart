23a8e04bfa371e58801d077762313a7b2a9e594e
farixdev <onlyfarix@gmail.com>
2026-04-17 20:45:46 +0500

    feat: refactor OnBoardingController methods for clarity and functionality

--- lib/features/authentication/controllers/onboarding/onboarding_controller.dart
+++ lib/features/authentication/controllers/onboarding/onboarding_controller.dart
@@ -1,22 +1,25 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class OnBoardingController extends GetxController {
   static OnBoardingController get instance => Get.find();
 
   ///variables
-  
+  final pageController = PageController();
+  Rx<int> currentPageIndex = 0.obs;
 
   //update current index whwen page changes
-  void updatePageIndicator(index){}
+  void updatePageIndicator(index) => currentPageIndex.value = index;
 
-  //jump to the specfic dot selected page 
-  void dotNavigationClick(index){}
+  //jump to the specfic dot selected page
+  void dotNavigationClick(index) {
+    currentPageIndex.value = index;
+    pageController.jumpTo(index);
+  }
 
   //Update current index and jump to the next page
-  void nextPage(){}
+  void nextPage() {}
 
   //Update current index and jump to the last page
-  void skipPage(){}
-
-
+  void skipPage() {}
 }
