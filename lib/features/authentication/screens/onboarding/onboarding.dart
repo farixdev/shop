9a493d12bc4a088d5df37b7ad92f8b81bb7e047b
farixdev <onlyfarix@gmail.com>
2026-04-17 20:15:11 +0500

    refactor: improve code readability in OnBoardingScreen by formatting PageView and updating imports

--- lib/features/authentication/screens/onboarding/onboarding.dart
+++ lib/features/authentication/screens/onboarding/onboarding.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:shop/features/authentication/screens/onboarding/widgets/onboarding_dot_navigation.dart';
 import 'package:shop/features/authentication/screens/onboarding/widgets/onboarding_page.dart';
 import 'package:shop/features/authentication/screens/onboarding/widgets/onboarding_skip.dart';
-import 'package:shop/utils/constants/image_strings.dart';
-import 'package:shop/utils/constants/text_strings.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
+import 'package:shop/utils/constants/image_strings.dart';
+
+import 'package:shop/utils/constants/text_strings.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
@@ -15,22 +16,35 @@ class OnBoardingScreen extends StatelessWidget {
       body: Stack(
         children: [
           ///horizontal scrollable page view with 3 pages
-          PageView(children: [
-            OnBoardingPage(image: FImages.onBoardingImage1, title: FTexts.onBoardingTitle1, subTitle: FTexts.onBoardingSubTitle1),
-            OnBoardingPage(image: FImages.onBoardingImage2, title: FTexts.onBoardingTitle2, subTitle: FTexts.onBoardingSubTitle2),
-            OnBoardingPage(image: FImages.onBoardingImage3, title: FTexts.onBoardingTitle3, subTitle: FTexts.onBoardingSubTitle3),
-          ]),
-
+          PageView(
+            children: [
+              OnBoardingPage(
+                image: FImages.onBoardingImage1,
+                title: FTexts.onBoardingTitle1,
+                subTitle: FTexts.onBoardingSubTitle1,
+              ),
+              OnBoardingPage(
+                image: FImages.onBoardingImage2,
+                title: FTexts.onBoardingTitle2,
+                subTitle: FTexts.onBoardingSubTitle2,
+              ),
+              OnBoardingPage(
+                image: FImages.onBoardingImage3,
+                title: FTexts.onBoardingTitle3,
+                subTitle: FTexts.onBoardingSubTitle3,
+              ),
+            ],
+          ),
 
           ///skip button
           const OnBoardingSkip(),
-          ///Dot naviagation
-          SmoothPageIndicator(controller: PageController(), count: 3)
-          ///Circular butoon
+
+          ///Dot navigation
+          const OnBoardingDotNavigation(),
+
+          ///Circular button
         ],
       ),
     );
   }
 }
-
-
