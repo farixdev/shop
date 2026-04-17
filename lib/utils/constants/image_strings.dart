a74b519cad5f2cb9f667d6f3d1aebe89b2bb516b
farixdev <onlyfarix@gmail.com>
2026-04-17 14:06:38 +0500

    fix: correct lightAppLogo variable name and ensure onboarding images are included

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -1,8 +1,14 @@
 class FImages {
   // app logos
-  static const String darkAppLogo = "assets/logos/f-store-splash-logo-black.png";
-  static const String larkAppLogo = "assets/logos/f-store-splash-logo-white.png";
+  static const String darkAppLogo =
+      "assets/logos/f-store-splash-logo-black.png";
+  static const String lightAppLogo =
+      "assets/logos/f-store-splash-logo-white.png";
 
-static const String google = "assets/logos/google-icon.png";
-static const String facebook = "assets/logos/facebook-icon.png";
+  static const String google = "assets/logos/google-icon.png";
+  static const String facebook = "assets/logos/facebook-icon.png";
+
+  static const String onBoardingImage1 = "assets/images/on_boarding_images/Ordering-Online-Food.gif";
+  static const String onBoardingImage2 = "assets/images/on_boarding_images/Tracking-Order-Online.gif";
+  static const String onBoardingImage3 = "assets/images/on_boarding_images/home-delivery.gif";
 }
