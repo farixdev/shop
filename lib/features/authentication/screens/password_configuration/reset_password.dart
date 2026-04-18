d2cd11415af6eeae2e371cef6f67c993fa1829ab
farixdev <onlyfarix@gmail.com>
2026-04-18 23:56:21 +0500

    feat: implement ResetPassword screen layout with navigation and button

--- lib/features/authentication/screens/password_configuration/reset_password.dart
+++ lib/features/authentication/screens/password_configuration/reset_password.dart
@@ -1,10 +1,65 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+import 'package:get/utils.dart';
+import 'package:shop/utils/constants/image_strings.dart';
+import 'package:shop/utils/constants/sizes.dart';
+import 'package:shop/utils/constants/text_strings.dart';
+import 'package:shop/utils/helpers/helper_functions.dart';
 
 class ResetPassword extends StatelessWidget {
   const ResetPassword({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            onPressed: () => Get.back,
+            icon: Icon(CupertinoIcons.clear),
+          ),
+        ],
+      ),
+      body:  SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(FSizes.defaultSpace),
+          child: Column(
+            children: [
+              //Image
+              Image(
+                image: const AssetImage(FImages.verifyEmailImage),
+                width: FHelperFunctions.screenWidth() * 0.6,
+              ),
+              const SizedBox(height: FSizes.defaultBtwSections),
+              //Title & Subtitle
+              Text(
+                FTexts.changeYourPasswordTitle,
+                style: Theme.of(context).textTheme.headlineMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: FSizes.defaultBtwItems),
+
+              Text(
+                FTexts.changeYourPasswordSubTitle,
+                style: Theme.of(context).textTheme.labelMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: FSizes.defaultBtwSections),
+
+              ///Buttons
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: (){},
+                  child: const Text(FTexts.fcontinue),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
