7098add31440493a8157b1a1ad3d9bcc1c38eabb
farixdev <onlyfarix@gmail.com>
2026-04-18 23:43:43 +0500

    fix: correct typos and improve formatting in onboarding and authentication texts

--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -6,9 +6,12 @@ class FTexts {
   static const String onBoardingTitle2 = "Select Payment Method";
   static const String onBoardingTitle3 = "Deliver At Your Door Step";
 
-  static const String onBoardingSubTitle1 = "Welcome to a World of Limitless Choices - Your Perfect Producet Awaits!";
-  static const String onBoardingSubTitle2 = "For Seamless Transaction, Choose Your Payment Path - Your Convenience, Our Priority!";
-  static const String onBoardingSubTitle3 = "From Our Doorstep to Yours - Swift, Secure adn Contactless Delivery!";
+  static const String onBoardingSubTitle1 =
+      "Welcome to a World of Limitless Choices - Your Perfect Producet Awaits!";
+  static const String onBoardingSubTitle2 =
+      "For Seamless Transaction, Choose Your Payment Path - Your Convenience, Our Priority!";
+  static const String onBoardingSubTitle3 =
+      "From Our Doorstep to Yours - Swift, Secure adn Contactless Delivery!";
 
   //authentication form texts
   static const String firstName = "First Name";
@@ -32,25 +35,26 @@ class FTexts {
   static const String resendEmailIn = "Resend Email in";
   static const String and = "and";
   static const String fcontinue = "Continue";
-  
-  
-  
+  static const String submit = "Submit";
 
   //authentication Heading Texts
   static const String loginTitle = "Welcome Back,";
-  static const String loginSubTitle = "Discover Limitless Choices and Unmatched Convenience.";
+  static const String loginSubTitle =
+      "Discover Limitless Choices and Unmatched Convenience.";
   static const String signupTitle = "Let's Create Your Account";
   static const String forgotPasswordTitle = "Forgot Password?";
-  static const String forgotPasswordSubTitle = "Don't Worry! It Happens to the best of us. Please Enter The Email and We'll Send You a password reset link.";
+  static const String forgotPasswordSubTitle =
+      "Don't Worry! It Happens to the best of us. Please Enter The Email and We'll Send You a password reset link.";
   static const String changeYourPasswordTitle = "Password reset email sent!";
-  static const String changeYourPasswordSubTitle = "Your Account Security is Our Priority. Please Check Your Email for the Password Reset Link and Follow the Instructions to Regain Access to Your Account.";
+  static const String changeYourPasswordSubTitle =
+      "Your Account Security is Our Priority. Please Check Your Email for the Password Reset Link and Follow the Instructions to Regain Access to Your Account.";
   static const String confirmEmail = "Verify Your Email address";
-  static const String confirmEmailSubTitle = "Congratulations! Your Account awaits: verify your email address to unlock a world of shopping possibilities. Click the link in your inbox to confirm your email and start exploring our exclusive offers and seamless shopping experience.";
-  static const String emailNotRecievedMessage = "Didn't receive the email? cbeck your spam folder or resend the email.";
-  static const String yourAccountCreatedTitle = "Your Account Has Been Created!";
-  static const String yourAccountCreatedSubTitle = "Welcome to your ultimale shopping destination! Your account has been successfully created. unleash the joy of seamless online shopping";
-  
-
-
-
+  static const String confirmEmailSubTitle =
+      "Congratulations! Your Account awaits: verify your email address to unlock a world of shopping possibilities. Click the link in your inbox to confirm your email and start exploring our exclusive offers and seamless shopping experience.";
+  static const String emailNotRecievedMessage =
+      "Didn't receive the email? cbeck your spam folder or resend the email.";
+  static const String yourAccountCreatedTitle =
+      "Your Account Has Been Created!";
+  static const String yourAccountCreatedSubTitle =
+      "Welcome to your ultimale shopping destination! Your account has been successfully created. unleash the joy of seamless online shopping";
 }
