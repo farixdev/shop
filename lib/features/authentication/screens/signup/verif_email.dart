aa5ff33f00690c3c6f32411908cde12420292bc3
farixdev <onlyfarix@gmail.com>
2026-04-18 22:58:14 +0500

    feat: add VerifyEmailScreen for email verification process

---
+++ lib/features/authentication/screens/signup/verif_email.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shop/features/authentication/screens/login/login.dart';
+import 'package:shop/utils/constants/image_strings.dart';
+ 
+import 'package:shop/utils/constants/sizes.dart';
+import 'package:shop/utils/constants/text_strings.dart';
+import 'package:shop/utils/helpers/helper_functions.dart';
+
+class VerifyEmailScreen extends StatelessWidget {
+  const VerifyEmailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(onPressed: () => Get.offAll(() => const LoginScreen()), icon: const Icon(CupertinoIcons.clear))
+        ],
+      ),
+      body:  SingleChildScrollView(
+        child: 
+        Padding(
+          padding:const EdgeInsets.all(FSizes.defaultSpace),
+          child: Column(
+            children: [
+              //Image
+              Image(image: AssetImage(FImages.verifyEmailImage), width: FHelperFunctions.screenWidth() * 0.6,),
+              const SizedBox(height: FSizes.defaultBtwSections),
+              //Title & Subtitle
+              Text(FTexts.confirmEmail, style: Theme.of(context).textTheme.headlineMedium, textAlign: TextAlign.center,),
+              const SizedBox(height: FSizes.defaultBtwItems), 
+              //will send email to user , add email dynamic later
+              Text('example@gmail.com', style: Theme.of(context).textTheme.labelLarge, textAlign: TextAlign.center,),
+              const SizedBox(height: FSizes.defaultBtwItems), 
+
+              Text(FTexts.confirmEmailSubTitle, style: Theme.of(context).textTheme.labelMedium, textAlign: TextAlign.center,),
+              const SizedBox(height: FSizes.defaultBtwSections), 
+
+              //Buttons 
+
+              SizedBox(width: double.infinity, child: ElevatedButton(onPressed: () {}, child: const Text(FTexts.fcontinue))),
+              const SizedBox(height: FSizes.defaultBtwItems),
+
+              SizedBox(width: double.infinity, child: TextButton(onPressed: () {}, child: const Text(FTexts.resendEmail))),
+            ],
+          ),
+        
+        
+        ),
+      ),
+    );
+  }
+}
