246c06b4484f0dd965d2f1a6cd6c6429c9f97bef
farixdev <onlyfarix@gmail.com>
2026-04-18 22:08:33 +0500

    feat: add terms and conditions checkbox widget for signup form

---
+++ lib/features/authentication/screens/signup/widgets/terms_conditions_checkbox.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+import 'package:shop/utils/constants/colors.dart';
+import 'package:shop/utils/constants/sizes.dart';
+import 'package:shop/utils/constants/text_strings.dart';
+import 'package:shop/utils/helpers/helper_functions.dart';
+
+class FTermsAndConditionsCheckBox extends StatelessWidget {
+  const FTermsAndConditionsCheckBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = FHelperFunctions.isDarkMode(context);
+    return Row(
+      children: [
+        SizedBox(
+          width: 24,
+          height: 24,
+          child: Checkbox(value: true, onChanged: (value) {}),
+        ),
+        const SizedBox(width: FSizes.defaultBtwItems),
+        Text.rich(
+          TextSpan(
+            children: [
+              TextSpan(
+                text: '${FTexts.iAgreeTo} ',
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+              TextSpan(
+                text: '${FTexts.privacyPolicy} ',
+                style: Theme.of(context).textTheme.bodyMedium!.apply(
+                  color: dark ? FColors.white : FColors.primaryColor,
+                  decoration: TextDecoration.underline,
+                  decorationColor: dark ? FColors.white : FColors.primaryColor,
+                ),
+              ),
+              TextSpan(
+                text: '${FTexts.and} ',
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+              TextSpan(
+                text: FTexts.termOfUse,
+                style: Theme.of(context).textTheme.bodyMedium!.apply(
+                  color: dark ? FColors.white : FColors.primaryColor,
+                  decoration: TextDecoration.underline,
+                  decorationColor: dark ? FColors.white : FColors.primaryColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
