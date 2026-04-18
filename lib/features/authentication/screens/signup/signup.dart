76c7a6dcdaac81d4d545cf3482e33fd31dad62f6
farixdev <onlyfarix@gmail.com>
2026-04-18 20:57:00 +0500

    implement SignupScreen layout with title and scrollable form structure

--- lib/features/authentication/screens/signup/signup.dart
+++ lib/features/authentication/screens/signup/signup.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:shop/utils/constants/sizes.dart';
+import 'package:shop/utils/constants/text_strings.dart';
 
 class SignupScreen extends StatelessWidget {
   const SignupScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsetsGeometry.all(FSizes.defaultSpace),
+          child: Column(
+            children: [
+              ///Title 
+              Text(FTexts.signupTitle, style: Theme.of(context).textTheme.headlineMedium),
+              
+              ///Form
+              
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
