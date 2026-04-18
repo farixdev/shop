56b6529c3aa0314014d1108baef6315ddfd73e4a
farixdev <onlyfarix@gmail.com>
2026-04-18 23:21:22 +0500

    feat: integrate SuccessScreen for account creation feedback

--- lib/features/authentication/screens/signup/verif_email.dart
+++ lib/features/authentication/screens/signup/verif_email.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shop/common/widgets/sucess_screen/sucess_screen.dart';
 import 'package:shop/features/authentication/screens/login/login.dart';
 import 'package:shop/utils/constants/image_strings.dart';
- 
+
 import 'package:shop/utils/constants/sizes.dart';
 import 'package:shop/utils/constants/text_strings.dart';
 import 'package:shop/utils/helpers/helper_functions.dart';
@@ -17,38 +18,71 @@ class VerifyEmailScreen extends StatelessWidget {
       appBar: AppBar(
         automaticallyImplyLeading: false,
         actions: [
-          IconButton(onPressed: () => Get.offAll(() => const LoginScreen()), icon: const Icon(CupertinoIcons.clear))
+          IconButton(
+            onPressed: () => Get.offAll(() => const LoginScreen()),
+            icon: const Icon(CupertinoIcons.clear),
+          ),
         ],
       ),
-      body:  SingleChildScrollView(
-        child: 
-        Padding(
-          padding:const EdgeInsets.all(FSizes.defaultSpace),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(FSizes.defaultSpace),
           child: Column(
             children: [
               //Image
-              Image(image: AssetImage(FImages.verifyEmailImage), width: FHelperFunctions.screenWidth() * 0.6,),
+              Image(
+                image: AssetImage(FImages.verifyEmailImage),
+                width: FHelperFunctions.screenWidth() * 0.6,
+              ),
               const SizedBox(height: FSizes.defaultBtwSections),
               //Title & Subtitle
-              Text(FTexts.confirmEmail, style: Theme.of(context).textTheme.headlineMedium, textAlign: TextAlign.center,),
-              const SizedBox(height: FSizes.defaultBtwItems), 
+              Text(
+                FTexts.confirmEmail,
+                style: Theme.of(context).textTheme.headlineMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: FSizes.defaultBtwItems),
               //will send email to user , add email dynamic later
-              Text('example@gmail.com', style: Theme.of(context).textTheme.labelLarge, textAlign: TextAlign.center,),
-              const SizedBox(height: FSizes.defaultBtwItems), 
-
-              Text(FTexts.confirmEmailSubTitle, style: Theme.of(context).textTheme.labelMedium, textAlign: TextAlign.center,),
-              const SizedBox(height: FSizes.defaultBtwSections), 
-
-              //Buttons 
-
-              SizedBox(width: double.infinity, child: ElevatedButton(onPressed: () {}, child: const Text(FTexts.fcontinue))),
+              Text(
+                'example@gmail.com',
+                style: Theme.of(context).textTheme.labelLarge,
+                textAlign: TextAlign.center,
+              ),
               const SizedBox(height: FSizes.defaultBtwItems),
 
-              SizedBox(width: double.infinity, child: TextButton(onPressed: () {}, child: const Text(FTexts.resendEmail))),
+              Text(
+                FTexts.confirmEmailSubTitle,
+                style: Theme.of(context).textTheme.labelMedium,
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: FSizes.defaultBtwSections),
+
+              //Buttons
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () => Get.to(
+                    () => SucessScreen(
+                      image: FImages.successImage,
+                      title: FTexts.yourAccountCreatedTitle, 
+                      subTitle: FTexts.yourAccountCreatedSubTitle,
+                      onPressed: () => Get.to(() => const LoginScreen()),
+                    ),
+                  ),
+                  child: const Text(FTexts.fcontinue),
+                ),
+              ),
+              const SizedBox(height: FSizes.defaultBtwItems),
+
+              SizedBox(
+                width: double.infinity,
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(FTexts.resendEmail),
+                ),
+              ),
             ],
           ),
-        
-        
         ),
       ),
     );
