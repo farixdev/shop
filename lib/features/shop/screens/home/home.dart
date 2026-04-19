1a3b981a43677355afbad5beef265491bbdc988d
farixdev <onlyfarix@gmail.com>
2026-04-19 22:35:51 +0500

    feat: simplify HomeScreen layout by removing unnecessary containers

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,47 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:shop/utils/constants/colors.dart';
+
+import 'package:shop/common/widgets/custom_shapes/containers/primary_header_container.dart';
+
+
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
-          children: [
-            Container(
-              color: FColors.primaryColor,
-              padding: const EdgeInsets.all(0),
-              child: Stack(
-                children: [
-                  Container(
-                    width: 400,
-                    height: 400,
-                    padding: const EdgeInsets.all(0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(400),
-                      // ignore: deprecated_member_use
-                      color: FColors.textWhite.withOpacity(0.1),
-                    ),
-                  ),
+          children: [FPrimaryHeaderContainer(child: Column(children: [
 
-                  Container(
-                    width: 400,
-                    height: 400,
-                    padding: const EdgeInsets.all(0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(400),
-                      // ignore: deprecated_member_use
-                      color: FColors.textWhite.withOpacity(0.1),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
+                
+              ]
+              ))],
         ),
       ),
     );
   }
 }
+
