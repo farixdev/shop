7391402f8e54f18851f734a231ca1450cb8fdd6f
farixdev <onlyfarix@gmail.com>
2026-04-19 23:49:01 +0500

    feat: add FAppBar widget for customizable app bar with optional back navigation

---
+++ lib/common/widgets/appbar/appbar.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
+import 'package:get/state_manager.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
+
+import 'package:shop/utils/constants/sizes.dart';
+import 'package:shop/utils/device/device_utility.dart';
+
+class FAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const FAppBar({
+    super.key,
+    this.title,
+    this.showBackArrow = false,
+    this.leadingIcon,
+    required this.actions,
+    this.leadingOnPress,
+  });
+
+  final Widget? title;
+  final bool showBackArrow;
+  final IconData? leadingIcon;
+  final List<Widget> actions;
+  final VoidCallback? leadingOnPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: FSizes.md),
+      child: AppBar(
+        automaticallyImplyLeading: false,
+        leading: showBackArrow
+            ? IconButton(
+                onPressed: () => Get.back,
+                icon: const Icon(Iconsax.arrow_left),
+              )
+            : leadingIcon != null
+            ? IconButton(onPressed: leadingOnPress, icon: Icon(leadingIcon))
+            : null,
+
+        title: title,
+        actions: actions,
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(FDeviceUtility.getAppBarHeight());
+}
