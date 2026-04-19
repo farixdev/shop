c2f1473001b007ac19cba81d683983459f12daab
farixdev <onlyfarix@gmail.com>
2026-04-19 11:48:33 +0500

    feat: implement reactive bottom navigation bar with NavigationController

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -1,19 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
+import 'package:shop/features/shop/screens/home/home.dart';
 
 class NavigationMenu extends StatelessWidget {
   const NavigationMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController()); 
+
     return Scaffold(
-      bottomNavigationBar: NavigationBar(
-        destinations: [
-          Container(color: Colors.green,),
-          Container(color: Colors.blue,),
-          Container(color: Colors.deepPurple,),
-          Container(color: Colors.orange,),
-        ]
+      bottomNavigationBar: Obx(
+        () =>  NavigationBar(
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected: (index) =>
+              controller.selectedIndex.value = index,
+          destinations: const [
+            NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Iconsax.shop), label: 'Store'),
+            NavigationDestination(icon: Icon(Iconsax.heart), label: 'Wishlist'),
+            NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
+          ],
         ),
+      ),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
     );
   }
+}
+
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
+
+  final screens = [
+    const HomeScreen(),
+    Container(color: Colors.blue),
+    Container(color: Colors.green),
+    Container(color: Colors.yellow),
+  ];
 }
