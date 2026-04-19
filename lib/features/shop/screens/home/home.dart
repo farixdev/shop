c61a14e809f067735dfe41eb154fd29d9ce7137f
farixdev <onlyfarix@gmail.com>
2026-04-19 13:36:27 +0500

    feat: implement basic layout for HomeScreen with scrollable content

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:shop/utils/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(   );
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: FColors.primaryColor,
+              padding: const EdgeInsets.all(0),
+              child: Stack(
+                children: [
+                  Container(
+                    width: 400,
+                    height: 400,
+                    padding: const EdgeInsets.all(0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(400),
+                      // ignore: deprecated_member_use
+                      color: FColors.textWhite.withOpacity(0.1),
+                    ),
+                  ),
+
+                  Container(
+                    width: 400,
+                    height: 400,
+                    padding: const EdgeInsets.all(0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(400),
+                      // ignore: deprecated_member_use
+                      color: FColors.textWhite.withOpacity(0.1),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
