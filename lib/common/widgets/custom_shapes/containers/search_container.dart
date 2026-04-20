055d726d0e476ca5f54d15796aaf9c46b6177750
farixdev <onlyfarix@gmail.com>
2026-04-21 00:12:10 +0500

    feat: make FSearchContainer tappable by adding onTap callback

--- lib/common/widgets/custom_shapes/containers/search_container.dart
+++ lib/common/widgets/custom_shapes/containers/search_container.dart
@@ -5,8 +5,6 @@ import 'package:shop/utils/constants/sizes.dart';
 import 'package:shop/utils/device/device_utility.dart';
 import 'package:shop/utils/helpers/helper_functions.dart';
 
-
-
 class FSearchContainer extends StatelessWidget {
   const FSearchContainer({
     super.key,
@@ -14,36 +12,41 @@ class FSearchContainer extends StatelessWidget {
     this.icon = Iconsax.search_normal,
     this.showBackground = true,
     this.showBorder = true,
+    this.ontap,
   });
 
   final String text;
   final IconData? icon;
   final bool showBackground, showBorder;
+  final VoidCallback? ontap;
 
   @override
   Widget build(BuildContext context) {
     final dark = FHelperFunctions.isDarkMode(context);
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: FSizes.defaultSpace),
-      child: Container(
-        width: FDeviceUtility.getScreenWidth(context),
-        padding: EdgeInsets.all(FSizes.md),
-        decoration: BoxDecoration(
-          color: showBackground
-              ? dark
-                    ? FColors.dark
-                    : FColors.light
-              : Colors.transparent,
-          borderRadius: BorderRadius.circular(FSizes.cardRadiusLg),
-          border: showBorder ? Border.all(color: FColors.grey) : null,
-        ),
-        child: Row(
-          children: [
-            Icon(icon, color: FColors.darkergrey),
-            const SizedBox(width: FSizes.defaultBtwItems),
-            Text(text, style: Theme.of(context).textTheme.bodySmall),
-          ],
+    return GestureDetector(
+      onTap: ontap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: FSizes.defaultSpace),
+        child: Container(
+          width: FDeviceUtility.getScreenWidth(context),
+          padding: EdgeInsets.all(FSizes.md),
+          decoration: BoxDecoration(
+            color: showBackground
+                ? dark
+                      ? FColors.dark
+                      : FColors.light
+                : Colors.transparent,
+            borderRadius: BorderRadius.circular(FSizes.cardRadiusLg),
+            border: showBorder ? Border.all(color: FColors.grey) : null,
+          ),
+          child: Row(
+            children: [
+              Icon(icon, color: FColors.darkergrey),
+              const SizedBox(width: FSizes.defaultBtwItems),
+              Text(text, style: Theme.of(context).textTheme.bodySmall),
+            ],
+          ),
         ),
       ),
     );
