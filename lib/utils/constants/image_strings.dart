a0193f2a67077cd4b4c58df3c0db135de81cb6d4
farixdev <onlyfarix@gmail.com>
2026-04-21 00:13:15 +0500

    refactor: organize onboarding and category image constants for better readability

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -8,10 +8,26 @@ class FImages {
   static const String google = "assets/logos/google-icon.png";
   static const String facebook = "assets/logos/facebook-icon.png";
 
-  static const String onBoardingImage1 = "assets/images/on_boarding_images/Ordering-Online-Food.gif";
-  static const String onBoardingImage2 = "assets/images/on_boarding_images/Tracking-Order-Online.gif";
-  static const String onBoardingImage3 = "assets/images/on_boarding_images/home-delivery.gif";
+  //onboarding images
+  static const String onBoardingImage1 =
+      "assets/images/on_boarding_images/Ordering-Online-Food.gif";
+  static const String onBoardingImage2 =
+      "assets/images/on_boarding_images/Tracking-Order-Online.gif";
+  static const String onBoardingImage3 =
+      "assets/images/on_boarding_images/home-delivery.gif";
 
-  static const String verifyEmailImage = "assets/images/animations/verify-email.png";
+  static const String verifyEmailImage =
+      "assets/images/animations/verify-email.png";
   static const String successImage = "assets/images/animations/success.png";
+
+  //carogeris image
+
+  static const String bowlingIcon = "assets/icons/categories/bowling.png";
+  static const String chairIcon = "assets/icons/categories/chair.png";
+  static const String cosemeticsIcon = "assets/icons/categories/cosmetics.png";
+  static const String diamondIcon = "assets/icons/categories/diamond.png";
+  static const String phoneIcon = "assets/icons/categories/smartphone.png";
+  static const String toyCarIcon = "assets/icons/categories/toy-car.png";
+  static const String womenShoesIcon =
+      "assets/icons/categories/women-shoes.png";
 }
