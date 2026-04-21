dc052142a7245a3c6b5985c5c07bbabacdf4611f
farixdev <onlyfarix@gmail.com>
2026-04-21 13:05:17 +0500

    feat: add FProductCardHorizontal widget for horizontal product display

---
+++ lib/common/widgets/products/product_cards/product_card_horizontal.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FProductCardHorizontal extends StatelessWidget {
+  const FProductCardHorizontal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
