bc3c9ade91d05c4176a1ebaba8792bc91e19c569
farixdev <onlyfarix@gmail.com>
2026-04-21 12:23:07 +0500

    fix: set default padding for FRoundedImage widget

--- lib/common/widgets/images/rounded-image.dart
+++ lib/common/widgets/images/rounded-image.dart
@@ -17,7 +17,7 @@ class FRoundedImage extends StatelessWidget {
     this.border,
     this.backgroundColor = FColors.light,
     this.fit = BoxFit.contain,
-    this.padding ,
+    this.padding = const EdgeInsets.all(0),
     this.isNetworkImage = false,
     this.onPressed,
     this.borderRadius = FSizes.md,
