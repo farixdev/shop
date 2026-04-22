cbb2ebf1795b7d4eecbc09da6d5ceb9049a824dc
farixdev <onlyfarix@gmail.com>
2026-04-22 20:41:20 +0500

    fix: update FAppBar to support dark mode styling for leading icon

--- lib/common/widgets/appbar/appbar.dart
+++ lib/common/widgets/appbar/appbar.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/extension_navigation.dart';
 import 'package:get/state_manager.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
+import 'package:shop/utils/constants/colors.dart';
 
 import 'package:shop/utils/constants/sizes.dart';
 import 'package:shop/utils/device/device_utility.dart';
+import 'package:shop/utils/helpers/helper_functions.dart';
 
 class FAppBar extends StatelessWidget implements PreferredSizeWidget {
   const FAppBar({
@@ -24,6 +26,7 @@ class FAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
+    final dark = FHelperFunctions.isDarkMode(context);
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: FSizes.md),
       child: AppBar(
@@ -31,7 +34,7 @@ class FAppBar extends StatelessWidget implements PreferredSizeWidget {
         leading: showBackArrow
             ? IconButton(
                 onPressed: () => Get.back,
-                icon: const Icon(Iconsax.arrow_circle_left),
+                icon: Icon(Iconsax.arrow_circle_left , color: dark? FColors.white : FColors.dark ),
               )
             : leadingIcon != null
             ? IconButton(onPressed: leadingOnPress, icon: Icon(leadingIcon))
