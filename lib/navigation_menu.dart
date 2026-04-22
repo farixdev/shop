aa6f9f3061820d09049e608df834dcec9075a3ff
farixdev <onlyfarix@gmail.com>
2026-04-22 16:11:24 +0500

    feat: add SettingScreen to NavigationController for user navigation

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
+import 'package:shop/features/personalization/screens/settings/setting.dart';
 import 'package:shop/features/shop/screens/home/home.dart';
 import 'package:shop/features/shop/screens/store/store.dart';
 import 'package:shop/features/shop/screens/wishlist/wishlist.dart';
@@ -47,6 +48,6 @@ class NavigationController extends GetxController {
     const HomeScreen(),
     const StoreScreen(),
     const FavouriteScreen(),
-    Container(color: Colors.yellow),
+    const SettingScreen(),
   ];
 }
