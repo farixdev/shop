df5c7acaaf7f27d78d2257ee4bfaef88cf06d1fa
farixdev <onlyfarix@gmail.com>
2026-04-22 08:55:51 +0500

    fix: correct reference to StoreScreen in NavigationController

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -44,7 +44,7 @@ class NavigationController extends GetxController {
 
   final screens = [
     const HomeScreen(),
-    const Store(),
+    const StoreScreen(),
     Container(color: Colors.green),
     Container(color: Colors.yellow),
   ];
