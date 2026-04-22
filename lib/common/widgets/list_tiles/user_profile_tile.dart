1d0f18b094827321182a2aa70c88eacea6356736
farixdev <onlyfarix@gmail.com>
2026-04-22 19:33:33 +0500

    feat: update FUserProfileTile to require onPressed callback for edit functionality

--- lib/common/widgets/list_tiles/user_profile_tile.dart
+++ lib/common/widgets/list_tiles/user_profile_tile.dart
@@ -1,14 +1,14 @@
-
 import 'package:flutter/material.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 import 'package:shop/common/widgets/images/circular_image.dart';
 import 'package:shop/utils/constants/colors.dart';
 import 'package:shop/utils/constants/image_strings.dart';
+
 class FUserProfileTile extends StatelessWidget {
-  const FUserProfileTile({
-    super.key,
-  });
+  const FUserProfileTile({super.key, required this.onPressed});
+
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -18,11 +18,23 @@ class FUserProfileTile extends StatelessWidget {
         width: 50,
         height: 50,
         padding: 0,
-        
-        ),
-        title: Text('Faris Ahmad' , style: Theme.of(  context).textTheme.headlineSmall!.apply(color: FColors.white)),
-        subtitle: Text('Faris@gmai.com' , style: Theme.of(  context).textTheme.bodyMedium!.apply(color: FColors.white)),
-        trailing: IconButton(onPressed: (){}, icon: Icon(Iconsax.edit, color: FColors.white  ,)),  
+      ),
+      title: Text(
+        'Faris Ahmad',
+        style: Theme.of(
+          context,
+        ).textTheme.headlineSmall!.apply(color: FColors.white),
+      ),
+      subtitle: Text(
+        'Faris@gmai.com',
+        style: Theme.of(
+          context,
+        ).textTheme.bodyMedium!.apply(color: FColors.white),
+      ),
+      trailing: IconButton(
+        onPressed: onPressed,
+        icon: Icon(Iconsax.edit, color: FColors.white),
+      ),
     );
   }
 }
