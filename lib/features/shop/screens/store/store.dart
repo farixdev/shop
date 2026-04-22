337f7f89a6e74f86516559e0b0e40bb74ace9cb0
farixdev <onlyfarix@gmail.com>
2026-04-22 09:07:12 +0500

    feat: enhance StoreScreen layout with search container and dynamic app bar styling

--- lib/features/shop/screens/store/store.dart
+++ lib/features/shop/screens/store/store.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:shop/common/widgets/appbar/appbar.dart';
+import 'package:shop/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:shop/common/widgets/products/cart/cart_menu_icon.dart';
+import 'package:shop/utils/constants/colors.dart';
+import 'package:shop/utils/constants/sizes.dart';
+import 'package:shop/utils/helpers/helper_functions.dart';
 
 class StoreScreen extends StatelessWidget {
   const StoreScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final dark = FHelperFunctions.isDarkMode(context);
     return Scaffold(
       appBar: FAppBar(
         title: Text('Store', style: Theme.of(context).textTheme.headlineMedium),
@@ -16,7 +21,25 @@ class StoreScreen extends StatelessWidget {
         headerSliverBuilder: (_, innerboxIsScrolled) {
           return[
               SliverAppBar(
+                automaticallyImplyLeading: false,
+                pinned: true,
+                floating: true,
+                backgroundColor: dark? FColors.black : FColors.white,
+                expandedHeight: 440,
                 
+                flexibleSpace: Padding(
+                  padding: EdgeInsets.all(FSizes.defaultSpace),
+                  child: ListView(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: [
+                      //--Search Bar--//
+                      SizedBox(height: FSizes.defaultBtwItems),
+                      FSearchContainer(text: " " , showBorder: true),
+                      SizedBox(height: FSizes.defaultBtwSections),
+                    ],
+                  ),
+                  ),
               ),
             ];
         },
