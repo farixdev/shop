202a4b53d5e52c9d2798c1c9dc4b33ccaf1e41e3
farixdev <onlyfarix@gmail.com>
2026-04-22 14:43:08 +0500

    feat: refactor FCategoryTab to improve layout structure and readability

--- lib/features/shop/screens/store/widgets/category_tab.dart
+++ lib/features/shop/screens/store/widgets/category_tab.dart
@@ -1,34 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:shop/common/widgets/brands/brand_show_case.dart';
+import 'package:shop/common/widgets/layouts/grid_layout.dart';
+import 'package:shop/common/widgets/products/product_cards/product_card_vertical.dart';
 import 'package:shop/common/widgets/texts/section_heading.dart';
 import 'package:shop/utils/constants/image_strings.dart';
 import 'package:shop/utils/constants/sizes.dart';
-
 
 class FCategoryTab extends StatelessWidget {
   const FCategoryTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-                padding: EdgeInsets.all(FSizes.defaultSpace),
-                child: Column(
-                  children: [
-                    ///--Brand
-                    FBrandShowcase(
-                      images: [
-                        FImages.tshirt1,
-                        FImages.sneakers1,
-                        FImages.watch1,
-                      ],
-                    ),
+    return ListView(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      children: [
+        Padding(
+          padding: EdgeInsets.all(FSizes.defaultSpace),
+          child: Column(
+            children: [
+              ///--Brand
+              FBrandShowcase(
+                images: [FImages.tshirt1, FImages.sneakers1, FImages.watch1],
+              ),
+              const SizedBox(height: FSizes.defaultBtwItems),
 
-                    ///--Products{You Might Like}
-                    FSectionHeading(title: 'You Might Like' , showActionButton: true, onPressed: (){}),
-                    const SizedBox(height: FSizes.defaultBtwItems,)
-                    
-                  ],
-                ),
-              );
+              ///--Products{You Might Like}
+              FSectionHeading(
+                title: 'You Might Like',
+                showActionButton: true,
+                onPressed: () {},
+              ),
+              const SizedBox(height: FSizes.defaultBtwItems),
+
+              FGridLayout(
+                itemCount: 4,
+                itemBuilder: (_, index) => FProductCardVertical(),
+              ),
+              const SizedBox(height: FSizes.defaultBtwSections),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
