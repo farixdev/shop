7a7a37d226720d2e9c25403d9d979dd0726ffe2e
farixdev <onlyfarix@gmail.com>
2026-04-22 12:50:31 +0500

    feat: refactor StoreScreen layout to improve structure and readability

--- lib/features/shop/screens/store/store.dart
+++ lib/features/shop/screens/store/store.dart
@@ -19,73 +19,82 @@ class StoreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dark = FHelperFunctions.isDarkMode(context);
-    return Scaffold(
-      appBar: FAppBar(
-        title: Text('Store', style: Theme.of(context).textTheme.headlineMedium),
-        actions: [FCartCounterIcon(onPressed: () {})],
-      ),
-      body: NestedScrollView(
-        headerSliverBuilder: (_, innerboxIsScrolled) {
-          return [
-            SliverAppBar(
-              automaticallyImplyLeading: false,
-              pinned: true,
-              floating: true,
-              backgroundColor: dark ? FColors.black : FColors.white,
-              expandedHeight: 440,
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: FAppBar(
+          title: Text(
+            'Store',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          actions: [FCartCounterIcon(onPressed: () {})],
+        ),
+        body: NestedScrollView(
+          headerSliverBuilder: (_, innerboxIsScrolled) {
+            return [
+              SliverAppBar(
+                automaticallyImplyLeading: false,
+                pinned: true,
+                floating: true,
+                backgroundColor: dark ? FColors.black : FColors.white,
+                expandedHeight: 440,
 
-              flexibleSpace: Padding(
-                padding: EdgeInsets.all(FSizes.defaultSpace),
-                child: ListView(
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  children: [
-                    //--Search Bar--//
-                    const SizedBox(height: FSizes.defaultBtwItems),
-                    FSearchContainer(
-                      text: "Search in Store ",
-                      showBorder: true,
-                      showBackground: false,
-                      padding: EdgeInsets.zero,
-                    ),
+                flexibleSpace: Padding(
+                  padding: EdgeInsets.all(FSizes.defaultSpace),
+                  child: ListView(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: [
+                      //--Search Bar--//
+                      const SizedBox(height: FSizes.defaultBtwItems),
+                      FSearchContainer(
+                        text: "Search in Store ",
+                        showBorder: true,
+                        showBackground: false,
+                        padding: EdgeInsets.zero,
+                      ),
 
-                    const SizedBox(height: FSizes.defaultBtwSections),
+                      const SizedBox(height: FSizes.defaultBtwSections),
 
-                    //feature Brand
-                    FSectionHeading(
-                      title: 'Feature Brand',
-                      showActionButton: true,
-                      onPressed: () {},
-                    ),
-                    const SizedBox(height: FSizes.defaultBtwItems / 1.5),
+                      //feature Brand
+                      FSectionHeading(
+                        title: 'Feature Brand',
+                        showActionButton: true,
+                        onPressed: () {},
+                      ),
+                      const SizedBox(height: FSizes.defaultBtwItems / 1.5),
 
-                    FGridLayout(
-                      itemCount: 4,
-                      mainAxisExtent: 80,
-                      itemBuilder: (_, index) {
-                        return FBrandCard(showBorder: false);
-                      },
-                    ),
+                      FGridLayout(
+                        itemCount: 4,
+                        mainAxisExtent: 80,
+                        itemBuilder: (_, index) {
+                          return FBrandCard(showBorder: false);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+
+                //Tabs
+                bottom: TabBar(
+                  isScrollable: true,
+                  indicatorColor: FColors.primaryColor,
+                  unselectedLabelColor: FColors.darkgrey,
+                  labelColor: dark ? FColors.white : FColors.primaryColor,
+
+                  tabs: [
+                    Tab(child: Text('Sports')),
+                    Tab(child: Text('Electronics')),
+                    Tab(child: Text('Fashion')),
+                    Tab(child: Text('Furniture')),
+                    Tab(child: Text('Beauty')),
                   ],
                 ),
               ),
-
-              //Tabs
-              bottom: TabBar(
-                isScrollable: true,
-                indicatorColor: FColors.primaryColor,
-                tabs: [
-                  Tab(child: Text('Sports')),
-                  Tab(child: Text('Electronics')),
-                  Tab(child: Text('Fashion')),
-                  Tab(child: Text('Furniture')),
-                  Tab(child: Text('Beauty')),
-                  ]
-                  ),
-            ),
-          ];
-        },
-        body: Container(),
+            ];
+          },
+          body: Container(),
+        ),
       ),
     );
   }
