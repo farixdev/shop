dd69d8a94011c8b70e161d36fad82e1e099b60d6
farixdev <onlyfarix@gmail.com>
2026-04-22 17:05:32 +0500

    feat: enhance SettingScreen with user profile and account settings layout

--- lib/features/personalization/screens/settings/setting.dart
+++ lib/features/personalization/screens/settings/setting.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
+
 import 'package:shop/common/widgets/appbar/appbar.dart';
 import 'package:shop/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:shop/common/widgets/list_tiles/setting_menu_tiles.dart';
+import 'package:shop/common/widgets/list_tiles/user_profile_tile.dart';
+import 'package:shop/common/widgets/texts/section_heading.dart';
+
 import 'package:shop/utils/constants/colors.dart';
+
 import 'package:shop/utils/constants/sizes.dart';
 
 class SettingScreen extends StatelessWidget {
@@ -25,15 +32,33 @@ class SettingScreen extends StatelessWidget {
                       ).textTheme.headlineMedium!.apply(color: FColors.white),
                     ),
                   ),
+                  //const SizedBox(height: FSizes.defaultBtwItems  ,),
+
+                  //User Profile Card
+                  FUserProfileTile(),
                   const SizedBox(height: FSizes.defaultBtwSections,)
                 ],
               ),
             ),
 
             ///-- Body
+            Padding(
+              padding: const EdgeInsets.all(FSizes.defaultSpace),
+              child: Column(
+                children: [
+                  // --Account Setting
+                  FSectionHeading(title: 'Account Setting'),
+                  const SizedBox(height: FSizes.defaultBtwItems,),
+                  SettingMenuTile(icon: Iconsax.safe_home, title: 'My Address', subTitle: 'Set shopping delivery address',),
+                  
+                ],
+              ),
+              
+              )
           ],
         ),
       ),
     );
   }
 }
+
