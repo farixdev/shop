a929af313fbb046e87061946cce60fdc698538e0
farixdev <onlyfarix@gmail.com>
2026-04-22 16:11:38 +0500

    feat: implement initial layout for SettingScreen with header

--- lib/features/personalization/screens/settings/setting.dart
+++ lib/features/personalization/screens/settings/setting.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:shop/common/widgets/appbar/appbar.dart';
+import 'package:shop/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:shop/utils/constants/colors.dart';
+import 'package:shop/utils/constants/sizes.dart';
 
 class SettingScreen extends StatelessWidget {
   const SettingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ///--- Header
+            FPrimaryHeaderContainer(
+              child: Column(
+                children: [
+                  FAppBar(
+                    title: Text(
+                      'Account',
+                      style: Theme.of(
+                        context,
+                      ).textTheme.headlineMedium!.apply(color: FColors.white),
+                    ),
+                  ),
+                  const SizedBox(height: FSizes.defaultBtwSections,)
+                ],
+              ),
+            ),
+
+            ///-- Body
+          ],
+        ),
+      ),
+    );
   }
 }
