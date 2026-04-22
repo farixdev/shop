78183dba43004183c23f3d013bb5480cfe9ddf6f
farixdev <onlyfarix@gmail.com>
2026-04-22 21:01:37 +0500

    feat: add rating display and layout for product details in ProductDetailScreen

--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 
 
 import 'package:shop/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
+import 'package:shop/utils/constants/sizes.dart';
 
 
 class ProductDetailScreen extends StatelessWidget {
@@ -19,6 +21,40 @@ class ProductDetailScreen extends StatelessWidget {
             FProductImageSlider(),
 
             ///2- Product Details
+            Padding(
+              padding: EdgeInsets.only(right: FSizes.defaultSpace, left: FSizes.defaultSpace,bottom: FSizes.defaultSpace),
+              child: Column(
+                children: [
+                  ///Rating & Share
+                  Row(
+                    children: [
+                      ///Rating
+                      Row(
+                        children: [
+                          Icon(Iconsax.star, color: Colors.amber, size: 24),
+                          SizedBox(width: FSizes.defaultBtwItems /2 ),
+                          Text.rich(
+                            TextSpan(
+                              children: [
+                                TextSpan(text: '5.0' , style: Theme.of(context).textTheme.bodyLarge,),
+                                const TextSpan(text: '(199)')
+                              ]
+                            )
+                          )
+                        ],
+                      )
+                      ///share button
+                    ],
+                  )
+                  ///Price, Title,Stock & Brand
+                  ///Attributes
+                  ///Checkout Button
+                  ///Description
+                  ///Reviews
+                ],
+              ),
+            
+            )
             
           ],
         ),
