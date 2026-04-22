edfe5477b4e6120e3003310fda6f249da38c1379
farixdev <onlyfarix@gmail.com>
2026-04-22 20:10:07 +0500

    feat: implement ProductDetailScreen with image slider and layout

---
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import 'package:shop/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import 'package:shop/utils/constants/colors.dart';
+import 'package:shop/utils/constants/image_strings.dart';
+import 'package:shop/utils/helpers/helper_functions.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  const ProductDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = FHelperFunctions.isDarkMode(context);
+    return  Scaffold(
+
+      body: SingleChildScrollView(
+        child: Column(
+            children: [
+              ///1-Product Image Slider
+              FCurveEdgeWidget(
+                child: Container(
+                  color: dark ? FColors.darkergrey : FColors.light,
+                  child: Stack(
+                    children: [
+                        //Main Large Image
+                        Image(image: AssetImage(FImages.watch1)),
+                    ],
+                  ),
+                ),
+              )
+            
+            ///2- Product Details
+            ],
+        ),
+      ),
+    );
+  }
+}
