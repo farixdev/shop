8e74cd1018d4e12e9a89165009b36eed13335dc4
farixdev <onlyfarix@gmail.com>
2026-04-22 17:05:27 +0500

    feat: add FUserProfileTile widget for user profile display

---
+++ lib/common/widgets/list_tiles/user_profile_tile.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
+
+import 'package:shop/common/widgets/images/circular_image.dart';
+import 'package:shop/utils/constants/colors.dart';
+import 'package:shop/utils/constants/image_strings.dart';
+class FUserProfileTile extends StatelessWidget {
+  const FUserProfileTile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: FCircularImage(
+        image: FImages.profilePicture,
+        width: 50,
+        height: 50,
+        padding: 0,
+        
+        ),
+        title: Text('Faris Ahmad' , style: Theme.of(  context).textTheme.headlineSmall!.apply(color: FColors.white)),
+        subtitle: Text('Faris@gmai.com' , style: Theme.of(  context).textTheme.bodyMedium!.apply(color: FColors.white)),
+        trailing: IconButton(onPressed: (){}, icon: Icon(Iconsax.edit, color: FColors.white  ,)),  
+    );
+  }
+}
