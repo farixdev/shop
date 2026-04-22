7c80aedccc1aade86b9c3aaa032d673dee766437
farixdev <onlyfarix@gmail.com>
2026-04-22 20:15:38 +0500

    fix: adjust layout of product image in ProductDetailScreen for better presentation

--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:shop/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
 import 'package:shop/utils/constants/colors.dart';
 import 'package:shop/utils/constants/image_strings.dart';
+import 'package:shop/utils/constants/sizes.dart';
 import 'package:shop/utils/helpers/helper_functions.dart';
 
 class ProductDetailScreen extends StatelessWidget {
@@ -11,26 +12,36 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final dark = FHelperFunctions.isDarkMode(context);
-    return  Scaffold(
-
+    return Scaffold(
       body: SingleChildScrollView(
         child: Column(
-            children: [
-              ///1-Product Image Slider
-              FCurveEdgeWidget(
-                child: Container(
-                  color: dark ? FColors.darkergrey : FColors.light,
-                  child: Stack(
-                    children: [
-                        //Main Large Image
-                        SizedBox(child: Image(image: AssetImage(FImages.watch1))),
-                    ],
-                  ),
+          children: [
+            ///1-Product Image Slider
+            FCurveEdgeWidget(
+              child: Container(
+                color: dark ? FColors.darkergrey : FColors.light,
+                child: Stack(
+                  children: [
+                    //Main Large Image
+                    SizedBox(
+                      height: 400,
+                      child: Padding(
+                        padding: EdgeInsets.all(FSizes.productImageRadius *2),
+                        child: Center(
+                          child: Image(image: AssetImage(FImages.watch1)),
+                        ),
+                      ),
+                    ),
+
+                    ///Image Slider
+                    
+                  ],
                 ),
-              )
-            
+              ),
+            ),
+
             ///2- Product Details
-            ],
+          ],
         ),
       ),
     );
