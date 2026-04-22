f131ed4cc90cdb865ee77954f0b0dd8c424e9cf6
farixdev <onlyfarix@gmail.com>
2026-04-22 14:27:57 +0500

    feat: add FCategoryTab widget for displaying brand showcase and product categories

---
+++ lib/features/shop/screens/store/widgets/category_tab.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:shop/common/widgets/brands/brand_show_case.dart';
+import 'package:shop/utils/constants/image_strings.dart';
+import 'package:shop/utils/constants/sizes.dart';
+
+
+class FCategoryTab extends StatelessWidget {
+  const FCategoryTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Padding(
+                padding: EdgeInsets.all(FSizes.defaultSpace),
+                child: Column(
+                  children: [
+                    ///--Brand
+                    FBrandShowcase(
+                      images: [
+                        FImages.tshirt1,
+                        FImages.sneakers1,
+                        FImages.watch1,
+                      ],
+                    ),
+
+                    ///--Products
+                    
+                  ],
+                ),
+              );
+  }
+}
