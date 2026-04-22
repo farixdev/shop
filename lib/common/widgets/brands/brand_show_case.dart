038c1f17df3ca1ce10c94491dcb9375ae1caaf51
farixdev <onlyfarix@gmail.com>
2026-04-22 14:27:47 +0500

    feat: add FBrandShowcase widget for displaying brand images and product count

---
+++ lib/common/widgets/brands/brand_show_case.dart
@@ -0,0 +1,58 @@
+
+import 'package:flutter/material.dart';
+
+import 'package:shop/common/widgets/brands/brand_card.dart';
+import 'package:shop/common/widgets/custom_shapes/containers/rounded_container.dart';
+
+
+import 'package:shop/utils/constants/colors.dart';
+
+
+import 'package:shop/utils/constants/sizes.dart';
+import 'package:shop/utils/helpers/helper_functions.dart';
+
+
+
+class FBrandShowcase extends StatelessWidget {
+  const FBrandShowcase({super.key, required this.images});
+
+  final List<String> images;
+  @override
+  Widget build(BuildContext context) {
+    
+    return FRoundedContainer(
+      showBorder: true,
+      borderColor: FColors.darkgrey,
+      backgroundColor: Colors.transparent,
+      padding: const EdgeInsets.all(FSizes.md),
+      margin: const EdgeInsets.only(bottom: FSizes.defaultBtwItems),
+      child: Column(
+        children: [
+          //Brand with product Count
+          const FBrandCard(showBorder: false , productCount: '250' ,),
+
+          //Brand top 3 profuct images
+          Row(
+            children: images.map((image) => brandTopProductImageWidget(image, context)).toList()
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget brandTopProductImageWidget(String image, context) {
+    final dark = FHelperFunctions.isDarkMode(context);
+    return Expanded(
+      child: FRoundedContainer(
+        height: 100,
+        padding: const EdgeInsets.all(FSizes.md),
+        margin: const EdgeInsets.only(right: FSizes.sm),
+        backgroundColor: dark ? FColors.darkergrey : FColors.light,
+        child:  Image(
+          fit: BoxFit.contain,
+          image: AssetImage(image),
+        ),
+      ),
+    );
+  }
+}
