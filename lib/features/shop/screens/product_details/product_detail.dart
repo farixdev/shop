9dfda6945eadc5944f15c01db963a16d7630410b
farixdev <onlyfarix@gmail.com>
2026-04-22 20:24:34 +0500

    fix: correct padding and add width, background color, and border to image slider in ProductDetailScreen

--- lib/features/shop/screens/product_details/product_detail.dart
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:shop/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import 'package:shop/common/widgets/images/rounded_image.dart';
 import 'package:shop/utils/constants/colors.dart';
 import 'package:shop/utils/constants/image_strings.dart';
 import 'package:shop/utils/constants/sizes.dart';
@@ -26,7 +27,7 @@ class ProductDetailScreen extends StatelessWidget {
                     SizedBox(
                       height: 400,
                       child: Padding(
-                        padding: EdgeInsets.all(FSizes.productImageRadius *2),
+                        padding: EdgeInsets.all(FSizes.productImageRadius * 2),
                         child: Center(
                           child: Image(image: AssetImage(FImages.watch1)),
                         ),
@@ -34,7 +35,18 @@ class ProductDetailScreen extends StatelessWidget {
                     ),
 
                     ///Image Slider
-                    
+                    ListView.separated(
+                      separatorBuilder: (_, __) =>
+                          const SizedBox(width: FSizes.defaultBtwItems),
+                      itemCount: 4,
+                      itemBuilder: (_, index) => FRoundedImage(
+                        width: 80,
+                        backgroundColor: dark ? FColors.dark : FColors.white,
+                        border: Border.all(color: FColors.primaryColor),
+                        padding: const EdgeInsets.all(FSizes.sm),
+                        imageUrl: FImages.tshirt1,
+                      ),
+                    ),
                   ],
                 ),
               ),
