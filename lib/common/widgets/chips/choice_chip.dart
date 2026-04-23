b7e9b2b853780dc7a012c1ef55ed6ff54a9be33c
farixdev <onlyfarix@gmail.com>
2026-04-23 14:04:17 +0500

    feat: add FChoiceChip widget for color-based selection

---
+++ lib/common/widgets/chips/choice_chip.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:shop/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:shop/utils/constants/colors.dart';
+import 'package:shop/utils/helpers/helper_functions.dart';
+
+class FChoiceChip extends StatelessWidget {
+  const FChoiceChip({
+    super.key,
+    required this.text,
+    required this.selected,
+    this.onSelected,
+  });
+
+  final String text;
+  final bool selected;
+  final void Function(bool)? onSelected;
+  @override
+  Widget build(BuildContext context) {
+    final isColor = FHelperFunctions.getColor(text) != null;
+    return ChoiceChip(
+      label: isColor ? SizedBox() : Text(''),
+      selected: selected,
+      onSelected: onSelected,
+      labelStyle: TextStyle(color: selected ? FColors.white : null),
+      avatar: isColor
+          ? FCircularContainer(
+              width: 50,
+              height: 50,
+              backgroundColor: FHelperFunctions.getColor(text)!,
+            )
+          : null,
+      shape: isColor
+          ? const CircleBorder()
+          : null,
+      labelPadding: isColor 
+          ? EdgeInsets.all(0)
+          : null,
+      padding: EdgeInsets.all(0),
+      //selectedColor: Colors.green,
+      // ignore: prefer_null_aware_operators
+      backgroundColor: isColor
+          ? FHelperFunctions.getColor(text)!
+          : null,
+    );
+  }
+}
