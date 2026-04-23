cb3713dc35ed161e151f3ef9b9209067f6731ab2
farixdev <onlyfarix@gmail.com>
2026-04-23 20:17:33 +0500

    feat: replace ProductReviewScreen implementation with a new structure for improved readability and maintainability

---
+++ lib/features/shop/screens/product_reviews/product_review.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:shop/common/widgets/appbar/appbar.dart';
+
+import 'package:shop/features/shop/screens/product_reviews/widgets/rating_progress_indicator.dart';
+
+import 'package:shop/utils/constants/sizes.dart';
+
+class ProductReviewScreen extends StatelessWidget {
+  const ProductReviewScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      ///App Bar
+      appBar: FAppBar(title: Text('Reviews & Rating'), showBackArrow: true),
+
+      ///body
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(FSizes.defaultSpace),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Rating and Reviews are verified adn are fro people who use the same type of device that you do",
+              ),
+              SizedBox(height: FSizes.defaultBtwItems),
+
+              // Overall Product Rating
+              FOverallProductRating(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
