a0c58bfa9d1c7660935241eb18c51f2162f7ff19
farixdev <onlyfarix@gmail.com>
2026-04-23 11:51:51 +0500

    feat: add circular image to brand section in FProductMetaData widget

--- lib/features/shop/screens/product_details/widgets/product_meta_data.dart
+++ lib/features/shop/screens/product_details/widgets/product_meta_data.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:shop/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:shop/common/widgets/images/circular_image.dart';
 import 'package:shop/common/widgets/texts/brand_title_with_verified_icon.dart';
 import 'package:shop/common/widgets/texts/product_price_text.dart';
 import 'package:shop/common/widgets/texts/product_title_text.dart';
 import 'package:shop/utils/constants/colors.dart';
 import 'package:shop/utils/constants/enums.dart';
+import 'package:shop/utils/constants/image_strings.dart';
 import 'package:shop/utils/constants/sizes.dart';
 
-//import 'package:shop/utils/helpers/helper_functions.dart';
+import 'package:shop/utils/helpers/helper_functions.dart';
 
 class FProductMetaData extends StatelessWidget {
   const FProductMetaData({super.key});
 
   @override
   Widget build(BuildContext context) {
-    //final dark = FHelperFunctions.isDarkMode(context);
+    final dark = FHelperFunctions.isDarkMode(context);
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -68,7 +70,18 @@ class FProductMetaData extends StatelessWidget {
         const SizedBox(width: FSizes.defaultBtwItems / 1.5),
 
         ///Brand
-        FBrandTitleTextWithVerifiedIcon(title: 'Nike' , brandTextSize: TextSizes.medium,)
+        Row(
+          children: [
+            FCircularImage(
+              image: FImages.cosemeticsIcon,
+              width: 32, height: 32,
+              overlayColor: dark? FColors.white : FColors.black,
+              
+              ),
+            FBrandTitleTextWithVerifiedIcon(title: 'Nike' , brandTextSize: TextSizes.medium,)
+          ],
+        )
+        
       ],
     );
   }
