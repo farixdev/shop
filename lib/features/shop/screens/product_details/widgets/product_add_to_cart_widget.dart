247c250bb28271e0fba269a4273c3a9fc52e01df
farixdev <onlyfarix@gmail.com>
2026-04-23 19:53:49 +0500

    feat: enhance FBottomAddToCart layout and styling with improved icon handling

--- lib/features/shop/screens/product_details/widgets/product_add_to_cart_widget.dart
+++ lib/features/shop/screens/product_details/widgets/product_add_to_cart_widget.dart
@@ -12,22 +12,49 @@ class FBottomAddToCart extends StatelessWidget {
   Widget build(BuildContext context) {
     final dark = FHelperFunctions.isDarkMode(context);
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: FSizes.defaultSpace, vertical: FSizes.defaultSpace / 2),
+      padding: const EdgeInsets.symmetric(
+        horizontal: FSizes.defaultSpace,
+        vertical: FSizes.defaultSpace / 2,
+      ),
       decoration: BoxDecoration(
-        color: dark? FColors.darkergrey : FColors.light,
+        color: dark ? FColors.darkergrey : FColors.light,
         borderRadius: BorderRadius.only(
           topLeft: Radius.circular(FSizes.cardRadiusLg),
-          topRight: Radius.circular(FSizes.cardRadiusLg), 
-        )
+          topRight: Radius.circular(FSizes.cardRadiusLg),
+        ),
       ),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween ,
         children: [
           Row(
             children: [
-              FCircularIcon(icon: Iconsax.minus_copy , width: 40, height: 40, backgroundColor: FColors.darkergrey, color: FColors.white,),
-              
+              FCircularIcon(
+                icon: Iconsax.minus_copy,
+                width: 40,
+                height: 40,
+                backgroundColor: FColors.darkergrey,
+                color: FColors.white,
+              ),
+              const SizedBox(width: FSizes.defaultBtwItems),
+              Text('2', style: Theme.of(context).textTheme.titleSmall),
+              const SizedBox(width: FSizes.defaultBtwItems),
+              FCircularIcon(
+                icon: Iconsax.add_copy,
+                width: 40,
+                height: 40,
+                backgroundColor: FColors.black,
+                color: FColors.white,
+              ),
             ],
-          )
+          ),
+          ElevatedButton(
+            onPressed: () {}, 
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.all(FSizes.md),
+              backgroundColor: FColors.black,
+              side: const BorderSide(color: FColors.black)
+            ),
+            child: const Text('Add to Cart')),
         ],
       ),
     );
