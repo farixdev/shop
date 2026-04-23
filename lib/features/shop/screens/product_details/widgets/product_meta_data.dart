46a8af1b6dc48581df2fe28f69095a1041c3fcd1
farixdev <onlyfarix@gmail.com>
2026-04-23 11:49:39 +0500

    feat: update FProductMetaData widget to include sale price and improve layout

--- lib/features/shop/screens/product_details/widgets/product_meta_data.dart
+++ lib/features/shop/screens/product_details/widgets/product_meta_data.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:shop/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:shop/common/widgets/texts/brand_title_with_verified_icon.dart';
+import 'package:shop/common/widgets/texts/product_price_text.dart';
+import 'package:shop/common/widgets/texts/product_title_text.dart';
 import 'package:shop/utils/constants/colors.dart';
+import 'package:shop/utils/constants/enums.dart';
 import 'package:shop/utils/constants/sizes.dart';
+
 //import 'package:shop/utils/helpers/helper_functions.dart';
 
 class FProductMetaData extends StatelessWidget {
@@ -17,28 +22,53 @@ class FProductMetaData extends StatelessWidget {
         Row(
           children: [
             //Sale Tag
-             FRoundedContainer(
-                      radius: FSizes.sm,
-                      // ignore: deprecated_member_use
-                      backgroundColor: FColors.secondary.withOpacity(0.8),
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: FSizes.sm,
-                        vertical: FSizes.xs,
-                      ),
-                      child:  Text(
-                        '20%',
-                        style: Theme.of(
-                          context,
-                        ).textTheme.labelLarge!.apply(color: FColors.black),
-                      ),
-                    ),
-                    const SizedBox(width: FSizes.defaultBtwItems,)
+            FRoundedContainer(
+              radius: FSizes.sm,
+              // ignore: deprecated_member_use
+              backgroundColor: FColors.secondary.withOpacity(0.8),
+              padding: const EdgeInsets.symmetric(
+                horizontal: FSizes.sm,
+                vertical: FSizes.xs,
+              ),
+              child: Text(
+                '20%',
+                style: Theme.of(
+                  context,
+                ).textTheme.labelLarge!.apply(color: FColors.black),
+              ),
+            ),
+            const SizedBox(width: FSizes.defaultBtwItems),
             //Price
+            Text(
+              'Rs250',
+              style: Theme.of(context).textTheme.titleSmall!.apply(
+                decoration: TextDecoration.lineThrough,
+              ),
+            ),
+            const SizedBox(width: FSizes.defaultBtwItems),
+            FProductPriceText(price: '175', isLarge: true),
           ],
-        )
+        ),
+
+        const SizedBox(width: FSizes.defaultBtwItems / 1.5),
+
         ///Title
+        FProductTitleText(title: 'Designed Printed T-Shirt'),
+        const SizedBox(width: FSizes.defaultBtwItems / 1.5),
+
         ///Stock Status
+        Row(
+          children: [
+            FProductTitleText(title: 'Status'),
+            const SizedBox(width: FSizes.defaultBtwItems),
+            Text('In Stock', style: Theme.of(context).textTheme.titleMedium),
+          ],
+        ),
+
+        const SizedBox(width: FSizes.defaultBtwItems / 1.5),
+
         ///Brand
+        FBrandTitleTextWithVerifiedIcon(title: 'Nike' , brandTextSize: TextSizes.medium,)
       ],
     );
   }
