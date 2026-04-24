76b9530b4a3f9dc448bc95025f362a96f3d8637f
farixdev <onlyfarix@gmail.com>
2026-04-24 22:57:25 +0500

    feat: simplify FBillingPaymentSection by removing subtotal, shipping, tax, and order total rows

--- lib/features/shop/screens/checkout/widgets/billing_payment_section.dart
+++ lib/features/shop/screens/checkout/widgets/billing_payment_section.dart
@@ -1,51 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:shop/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:shop/common/widgets/texts/section_heading.dart';
+import 'package:shop/utils/constants/colors.dart';
+import 'package:shop/utils/constants/image_strings.dart';
 import 'package:shop/utils/constants/sizes.dart';
+import 'package:shop/utils/helpers/helper_functions.dart';
 
 class FBillingPaymentSection extends StatelessWidget {
   const FBillingPaymentSection({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final dark = FHelperFunctions.isDarkMode(context);
     return Column(
       children: [
-        //subtotal
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text('Subtotal', style: Theme.of(context).textTheme.bodyMedium,),
-            Text('Rs1234', style: Theme.of(context).textTheme.bodyMedium,),
-          ],
+        FSectionHeading(
+          title: 'Payment Method',
+          buttonTitle: 'Change',
+          showActionButton: true,
+          onPressed: (){},
         ),
         const SizedBox(height: FSizes.defaultBtwItems / 2),
-
-         //Shipping Fee
         Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text('Shipping Fee', style: Theme.of(context).textTheme.bodyMedium,),
-            Text('Rs200', style: Theme.of(context).textTheme.labelLarge,),
+            FRoundedContainer(
+              width: 60,
+              height: 35,
+              backgroundColor: dark? FColors.light : FColors.white ,
+              padding: EdgeInsets.all(FSizes.sm),
+              child: Image(image:  AssetImage(FImages.facebook) , fit: BoxFit.contain,),
+            ),
+            const SizedBox(height: FSizes.defaultBtwItems / 2),
+            Text('PayPal' , style: Theme.of(context).textTheme.bodyLarge,)
           ],
-        ),
-
-          //Tax Fee
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text('Tax Fee', style: Theme.of(context).textTheme.bodyMedium,),
-            Text('Rs70', style: Theme.of(context).textTheme.labelLarge,),
-          ],
-        ),
-        const SizedBox(height: FSizes.defaultBtwItems / 2),
-
-        
-          //Order Total
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text('Order Total ', style: Theme.of(context).textTheme.bodyMedium,),
-            Text('Rs70', style: Theme.of(context).textTheme.titleMedium,),
-          ],
-        ),
+        )
       ],
     );
   }
