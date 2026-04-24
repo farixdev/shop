809c7a28efc4483b7571d318b3f0e59f2e5fd160
farixdev <onlyfarix@gmail.com>
2026-04-24 22:57:19 +0500

    feat: add FBillingAmmountSection widget for displaying billing amounts in CheckoutScreen

---
+++ lib/features/shop/screens/checkout/widgets/billing_amount_section.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:shop/utils/constants/sizes.dart';
+
+class FBillingAmmountSection extends StatelessWidget {
+  const FBillingAmmountSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        //subtotal
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Subtotal', style: Theme.of(context).textTheme.bodyMedium,),
+            Text('Rs1234', style: Theme.of(context).textTheme.bodyMedium,),
+          ],
+        ),
+        const SizedBox(height: FSizes.defaultBtwItems / 2),
+
+         //Shipping Fee
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Shipping Fee', style: Theme.of(context).textTheme.bodyMedium,),
+            Text('Rs200', style: Theme.of(context).textTheme.labelLarge,),
+          ],
+        ),
+
+          //Tax Fee
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Tax Fee', style: Theme.of(context).textTheme.bodyMedium,),
+            Text('Rs70', style: Theme.of(context).textTheme.labelLarge,),
+          ],
+        ),
+        const SizedBox(height: FSizes.defaultBtwItems / 2),
+
+        
+          //Order Total
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('Order Total ', style: Theme.of(context).textTheme.bodyMedium,),
+            Text('Rs70', style: Theme.of(context).textTheme.titleMedium,),
+          ],
+        ),
+      ],
+    );
+  }
+}
