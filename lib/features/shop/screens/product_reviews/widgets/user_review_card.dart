87e79ac8c8bc8e2741cdc19d30289e33b2466a62
farixdev <onlyfarix@gmail.com>
2026-04-24 10:01:20 +0500

    feat: enhance Company Review Reply section with improved layout and ReadMoreText for better readability

--- lib/features/shop/screens/product_reviews/widgets/user_review_card.dart
+++ lib/features/shop/screens/product_reviews/widgets/user_review_card.dart
@@ -64,21 +64,46 @@ class UserReviewCard extends StatelessWidget {
         //Company Review Reply
         FRoundedContainer(
           backgroundColor: dark ? FColors.darkergrey : FColors.grey,
-          child: Padding(padding: EdgeInsets.all(FSizes.md),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text("F's Store " , style: Theme.of(context).textTheme.bodyLarge,),
-                  Text("01 Nov, 2026" , style: Theme.of(context).textTheme.bodyMedium,),
-                ],
-              )
-            ],
-          ),
-          
+          child: Padding(
+            padding: EdgeInsets.all(FSizes.md),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "F's Store ",
+                      style: Theme.of(context).textTheme.bodyLarge,
+                    ),
+                    Text(
+                      "01 Nov, 2026",
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: FSizes.defaultBtwItems),
+                ReadMoreText(
+                  'The User of this app is quite intuitive. I was able to navigate and make purchase seamlessly. Great job!',
+                  trimLines: 1,
+                  trimMode: TrimMode.Line,
+                  trimExpandedText: ' show less',
+                  trimCollapsedText: ' show more',
+                  moreStyle: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: FColors.fprimaryColor,
+                  ),
+                  lessStyle: const TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500,
+                    color: FColors.fprimaryColor,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
+        const SizedBox(height: FSizes.defaultBtwSections),     
       ],
     );
   }
