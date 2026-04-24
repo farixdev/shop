cc8c24a31c5c9d69a439726d5aeabbfe6b26a8ab
farixdev <onlyfarix@gmail.com>
2026-04-24 10:36:07 +0500

    feat: update UserAddressScreen layout and icon for improved UI consistency

--- lib/features/personalization/screens/address/address.dart
+++ lib/features/personalization/screens/address/address.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
 import 'package:shop/common/widgets/appbar/appbar.dart';
+
 import 'package:shop/features/personalization/screens/address/add_new_address.dart';
+import 'package:shop/features/personalization/screens/address/widgets/single_address.dart';
 import 'package:shop/utils/constants/colors.dart';
+import 'package:shop/utils/constants/sizes.dart';
 
 class UserAddressScreen extends StatelessWidget {
   const UserAddressScreen({super.key});
@@ -13,12 +16,23 @@ class UserAddressScreen extends StatelessWidget {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () => Get.to(() => AddNewAddressScreen()),
-        child: Icon(Iconsax.add_circle , color: FColors.white,),
         backgroundColor: FColors.fprimaryColor,
+        child: Icon(Iconsax.add_copy , color: FColors.white,),
       ),
       appBar: FAppBar(
         showBackArrow: true,
         title: Text('Addresses' , style: Theme.of(context).textTheme.headlineSmall  ,),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(FSizes.defaultSpace),
+          child: Column(
+            children: [
+             FSingleAddress(selectedAddress: true)
+            ],
+          ),
+
+        ),
       ),
     );
   }
