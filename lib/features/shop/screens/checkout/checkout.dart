99a9cf5f2c20b70c2ac78031234a3492ad6cd42c
farixdev <onlyfarix@gmail.com>
2026-04-24 22:31:51 +0500

    refactor: simplify FCouponCode widget by removing dark mode dependency

--- lib/features/shop/screens/checkout/checkout.dart
+++ lib/features/shop/screens/checkout/checkout.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:shop/common/widgets/appbar/appbar.dart';
-import 'package:shop/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:shop/common/widgets/products/cart/coupon_widget.dart';
+
 import 'package:shop/features/shop/screens/cart/widgets/cart_items.dart';
-import 'package:shop/utils/constants/colors.dart';
+
 import 'package:shop/utils/constants/sizes.dart';
-import 'package:shop/utils/helpers/helper_functions.dart';
+//import 'package:shop/utils/helpers/helper_functions.dart';
 
 class CheckoutScreen extends StatelessWidget {
   const CheckoutScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final dark = FHelperFunctions.isDarkMode(context);
+    //final dark = FHelperFunctions.isDarkMode(context);
     return Scaffold(
       appBar: FAppBar(
         showBackArrow: true,
@@ -30,7 +31,7 @@ class CheckoutScreen extends StatelessWidget {
               const SizedBox(height: FSizes.defaultBtwSections),
 
               //Coupon TextField
-              FCouponCode(dark: dark),
+              FCouponCode(),
               const SizedBox(height: FSizes.defaultBtwSections),
             ],
           ),
@@ -40,58 +41,3 @@ class CheckoutScreen extends StatelessWidget {
   }
 }
 
-class FCouponCode extends StatelessWidget {
-  const FCouponCode({
-    super.key,
-    required this.dark,
-  });
-
-  final bool dark;
-
-  @override
-  Widget build(BuildContext context) {
-    return FRoundedContainer(
-      showBorder: true,
-      backgroundColor: dark ? FColors.dark : FColors.white,
-      padding: EdgeInsets.only(
-        top: FSizes.sm,
-        bottom: FSizes.sm,
-        left: FSizes.md,
-        right: FSizes.sm,
-      ),
-      child: Row(
-        children: [
-          Flexible(
-            child: TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Have a promo code? Enter here',
-                focusedBorder: InputBorder.none,
-                border: InputBorder.none,
-                enabledBorder: InputBorder.none,
-                disabledBorder: InputBorder.none,
-                errorBorder: InputBorder.none,
-              ),
-            ),
-          ),
-          //Button
-          SizedBox(
-            width: 80,
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                // ignore: deprecated_member_use
-                foregroundColor: dark?  FColors.white.withOpacity(0.5) : FColors.dark.withOpacity(0.5),
-                // ignore: deprecated_member_use
-                backgroundColor: FColors.grey.withOpacity(0.2), 
-                // ignore: deprecated_member_use
-                side: BorderSide(color: FColors.grey.withOpacity(0.1)),
-    
-              ),
-              child: Text('Apply'),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
