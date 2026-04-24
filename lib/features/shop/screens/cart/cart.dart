1c9c57404bf53852e2d2fe871b52fbb9f8e9626c
farixdev <onlyfarix@gmail.com>
2026-04-24 20:53:01 +0500

    feat: refactor CartScreen layout to include product quantity management and improve structure

--- lib/features/shop/screens/cart/cart.dart
+++ lib/features/shop/screens/cart/cart.dart
@@ -1,52 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax_flutter/iconsax_flutter.dart';
 import 'package:shop/common/widgets/appbar/appbar.dart';
-import 'package:shop/common/widgets/icons/f-circular-icon.dart';
+
+import 'package:shop/common/widgets/products/cart/add_remove_button.dart';
 import 'package:shop/common/widgets/products/cart/cart_item.dart';
-import 'package:shop/utils/constants/colors.dart';
+import 'package:shop/common/widgets/texts/product_price_text.dart';
 
 import 'package:shop/utils/constants/sizes.dart';
-import 'package:shop/utils/helpers/helper_functions.dart';
+//import 'package:shop/utils/helpers/helper_functions.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final dark = FHelperFunctions.isDarkMode(context);
+    //final dark = FHelperFunctions.isDarkMode(context);
     return Scaffold(
       appBar: FAppBar(
         showBackArrow: true,
         title: Text('Cart', style: Theme.of(context).textTheme.headlineMedium),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(FSizes.defaultSpace),
-          child: ListView.separated(
-            shrinkWrap: true,
-            separatorBuilder: (_, _) =>
-                SizedBox(height: FSizes.defaultBtwSections),
-            itemCount: 4,
-            itemBuilder: (_, index) => Column(
-              children: [
-                FCartItem(),
-                SizedBox(height: FSizes.defaultBtwItems),
-                Row(
-                  children: [
-                    //Add & Remove Button
-                    FCircularIcon(
-                      icon: Iconsax.minus_copy,
-                      width: 32,
-                      height: 32,
-                      size: FSizes.md,
-                      color: dark? FColors.white : FColors.black,
-                    ),
-                  ],
-                ),
-              ],
-            ),
+      body: Padding(
+        padding: EdgeInsets.all(FSizes.defaultSpace),
+        child: ListView.separated(
+          shrinkWrap: true,
+          separatorBuilder: (_, _) =>
+              SizedBox(height: FSizes.defaultBtwSections),
+          itemCount: 10,
+          itemBuilder: (_, index) => Column(
+            children: [
+              FCartItem(),
+              SizedBox(height: FSizes.defaultBtwItems),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      const SizedBox(width: 70),
+                      //Add & Remove Button
+                      FProductQuantityWithAddRemove(),
+                    ],
+                  ),
+
+                  FProductPriceText(price: '120'),
+                ],
+              ),
+            ],
           ),
         ),
+      ),
+
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.all(FSizes.defaultSpace),
+        child: ElevatedButton(onPressed: () {}, child: Text('Checkout Rs1230')),
       ),
     );
   }
