fa24e87f8975edf0f5936681e229eced864660b1
farixdev <onlyfarix@gmail.com>
2026-04-25 09:21:47 +0500

    feat: update FHomeCategory to navigate to SubCategoriesScreen on tap

--- lib/features/shop/screens/home/widgets/home_categories.dart
+++ lib/features/shop/screens/home/widgets/home_categories.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/src/extension_navigation.dart';
 
 import 'package:shop/common/widgets/image_text/vertical_image_text.dart';
+import 'package:shop/features/shop/screens/sub_category/sub_categories.dart';
 
 
 
 class FHomeCategory extends StatelessWidget {
-  const FHomeCategory({super.key, required this.categories});
+  const  FHomeCategory({super.key, required this.categories});
 
   final List<Map<String, String>> categories;
 
@@ -21,7 +24,7 @@ class FHomeCategory extends StatelessWidget {
           return FVerticalImageText(
             image: categories[index]['image']!,
             title: categories[index]['title']!,
-            onTap: () {},
+            onTap: () => Get.to(() => SubCategoriesScreen()),
           );
         },
       ),
