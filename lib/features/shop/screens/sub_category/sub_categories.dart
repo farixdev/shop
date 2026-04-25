ef0297cbbac740dba9ea532be4884e8ae2569d0c
farixdev <onlyfarix@gmail.com>
2026-04-25 09:21:52 +0500

    feat: add SubCategoriesScreen with layout and banner

---
+++ lib/features/shop/screens/sub_category/sub_categories.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:shop/common/widgets/appbar/appbar.dart';
+import 'package:shop/common/widgets/images/rounded_image.dart';
+import 'package:shop/common/widgets/texts/section_heading.dart';
+import 'package:shop/utils/constants/image_strings.dart';
+import 'package:shop/utils/constants/sizes.dart';
+
+class SubCategoriesScreen extends StatelessWidget {
+  const SubCategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: FAppBar(title: Text('Sport Shirts'), showBackArrow: true),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(FSizes.defaultSpace) ,
+          child: Column(
+            children: [
+              //Banner
+              FRoundedImage(imageUrl: FImages.shoeBanner , width: double.infinity , height: null, applyImageRadius: true ,),
+              const SizedBox(height: FSizes.defaultBtwSections,),
+
+              //Sub Categoies
+              Column(
+                children: [
+                  ///Heading
+                  FSectionHeading(title: 'Sports Shirt' , onPressed: (){}, showActionButton: true,),
+                  const SizedBox(height: FSizes.defaultBtwItems / 2 ),
+
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
