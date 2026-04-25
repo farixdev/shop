a959e8d674bc597205a7c8f6c396a23325de1fb6
farixdev <onlyfarix@gmail.com>
2026-04-25 08:54:21 +0500

    feat: implement FBillingAddressSection with shipping address details and layout

--- lib/features/shop/screens/checkout/widgets/billing_address_section.dart
+++ lib/features/shop/screens/checkout/widgets/billing_address_section.dart
@@ -1,10 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
+import 'package:shop/common/widgets/texts/section_heading.dart';
+import 'package:shop/utils/constants/colors.dart';
+import 'package:shop/utils/constants/sizes.dart';
 
 class FBillingAddressSection extends StatelessWidget {
   const FBillingAddressSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        FSectionHeading(
+          title: 'Shipping Address',
+          buttonTitle: 'Change',
+          showActionButton: true,
+          onPressed: (){},
+        ),
+        Text('Faris' , style: Theme.of(context).textTheme.bodyLarge,),
+
+        Row(
+          children: [
+            const Icon(Iconsax.mobile, color: FColors.grey, size: 16,),
+            const SizedBox(width: FSizes.defaultBtwItems,),
+            Text('+923456789', style: Theme.of(context).textTheme.bodyMedium,)
+          ],
+        ),
+        const SizedBox(width: FSizes.defaultBtwItems/2),
+
+        Row(
+          children: [
+            const Icon(Iconsax.location, color: FColors.grey, size: 16,),
+            const SizedBox(width: FSizes.defaultBtwItems,),
+            Expanded(child: Text('123 Main Street, New York', style: Theme.of(context).textTheme.bodyMedium, softWrap: true ,))
+          ],
+        ),
+
+
+      ],
+    );
   }
 }
