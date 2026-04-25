e8d618c33225059f40a870a30cda80f649403381
farixdev <onlyfarix@gmail.com>
2026-04-25 08:54:03 +0500

    feat: create OrderScreen widget for displaying user orders

---
+++ lib/features/shop/order/order.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:shop/common/widgets/appbar/appbar.dart';
+import 'package:shop/features/shop/order/widgets/order_list.dart';
+import 'package:shop/utils/constants/sizes.dart';
+
+class OrderScreen extends StatelessWidget {
+  const OrderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: FAppBar(title: Text('My Order' , style: Theme.of(context).textTheme.headlineSmall,), showBackArrow: true,),
+      body: Padding(
+        padding: EdgeInsets.all(FSizes.defaultSpace),
+
+        //Orders
+        child: FOrderListItems() ,
+      
+      ),
+    );
+  }
+}
