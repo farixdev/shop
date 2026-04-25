071485ce73176906f4f47d377920632be3d51228
farixdev <onlyfarix@gmail.com>
2026-04-25 11:22:40 +0500

    feat: replace product display with FSortableProducts for enhanced sorting functionality

--- lib/features/shop/screens/all_products/all_products.dart
+++ lib/features/shop/screens/all_products/all_products.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax_flutter/iconsax_flutter.dart';
+
 import 'package:shop/common/widgets/appbar/appbar.dart';
-import 'package:shop/common/widgets/layouts/grid_layout.dart';
-import 'package:shop/common/widgets/products/product_cards/product_card_vertical.dart';
+
+import 'package:shop/common/widgets/products/sortable/sortable_products.dart';
 import 'package:shop/utils/constants/sizes.dart';
 
 class AllProduct extends StatelessWidget {
@@ -14,41 +14,9 @@ class AllProduct extends StatelessWidget {
       appBar: FAppBar(title: Text('Popular Products'), showBackArrow: true),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(FSizes.defaultSpace),
-        child: Column(
-          children: [
-            //Drop Down
-            DropdownButtonFormField(
-              decoration: InputDecoration(prefixIcon: Icon(Iconsax.sort)),
-              onChanged: (value) {},
-              items:
-                  [
-                        'Name',
-                        'Higher Price',
-                        'Lower Price',
-                        'Sale',
-                        'Newest',
-                        'Popularity',
-                      ]
-                      .map(
-                        (option) => DropdownMenuItem(
-                          value: option,
-
-                          child: Text(option),
-                        ),
-                      )
-                      .toList(),
-            ),
-
-            const SizedBox(height: FSizes.defaultBtwSections), 
-
-            //Products
-            FGridLayout(
-              itemCount: 6, 
-              itemBuilder: (_, index) => FProductCardVertical()
-              )
-          ],
-        ),
+        child: FSortableProducts(),
       ),
     );
   }
 }
+
