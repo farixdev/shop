8a1a0f6024c3cbb684bd6296b0e585332eb4cb5d
farixdev <onlyfarix@gmail.com>
2026-04-25 11:36:08 +0500

    feat: add onTap functionality to FBrandCard for navigation to BrandProducts

--- lib/features/shop/screens/brand/all_brands.dart
+++ lib/features/shop/screens/brand/all_brands.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:shop/common/widgets/appbar/appbar.dart';
 import 'package:shop/common/widgets/brands/brand_card.dart';
 import 'package:shop/common/widgets/layouts/grid_layout.dart';
+
 import 'package:shop/common/widgets/texts/section_heading.dart';
+import 'package:shop/features/shop/screens/brand/brand_products.dart';
 import 'package:shop/utils/constants/sizes.dart';
 
 class AllBrandsScreen extends StatelessWidget {
@@ -25,7 +28,7 @@ class AllBrandsScreen extends StatelessWidget {
             FGridLayout(
               itemCount: 6,
               mainAxisExtent: 80,
-             itemBuilder: (context , index) =>FBrandCard(showBorder: true,)
+             itemBuilder: (context , index) =>FBrandCard(showBorder: true, onTap: ()=> Get.to(()=>BrandProducts()),)
              
              )
           ],
