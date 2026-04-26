84904fe3ead9367dcc1e83b5f25f646239e45daf
farixdev <onlyfarix@gmail.com>
2026-04-26 21:40:55 +0500

    feat: enhance FSignupForm with first name, last name, email, phone number, and password validation

--- lib/features/authentication/screens/signup/widgets/signup_form.dart
+++ lib/features/authentication/screens/signup/widgets/signup_form.dart
@@ -1,27 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_navigation/get_navigation.dart';
+import 'package:get/get.dart';
+
+
 
 import 'package:iconsax_flutter/iconsax_flutter.dart';
-import 'package:shop/features/authentication/screens/signup/verif_email.dart';
+import 'package:shop/features/authentication/controllers/signup/signup_controller.dart';
+
 import 'package:shop/features/authentication/screens/signup/widgets/terms_conditions_checkbox.dart';
 
 import 'package:shop/utils/constants/sizes.dart';
 import 'package:shop/utils/constants/text_strings.dart';
+import 'package:shop/utils/validators/validation.dart';
 
 class FSignupForm extends StatelessWidget {
   const FSignupForm({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(SignupController());
     return Form(
+      key: controller.signupFormKey,
       child: Column(
         children: [
           Row(
             children: [
               Expanded(
                 child: TextFormField(
+                  controller: controller.firstName,
                   expands: false,
+                  validator: (value) => FValidator.vlidateEmptyText('First Name', value),
                   decoration: const InputDecoration(
                     label: Text(FTexts.firstName),
                     prefixIcon: Icon(Iconsax.user),
@@ -31,6 +38,8 @@ class FSignupForm extends StatelessWidget {
               const SizedBox(width: FSizes.spaceBtwInputFields),
               Expanded(
                 child: TextFormField(
+                  controller: controller.lastName,
+                  validator: (value) => FValidator.vlidateEmptyText('Last Name', value),
                   expands: false,
                   decoration: const InputDecoration(
                     label: Text(FTexts.lastName),
@@ -44,7 +53,9 @@ class FSignupForm extends StatelessWidget {
 
           ///Username
           TextFormField(
+            controller: controller.username,
             expands: false,
+            validator: (value) => FValidator.vlidateEmptyText('Username', value),
             decoration: const InputDecoration(
               label: Text(FTexts.username),
               prefixIcon: Icon(Iconsax.user_edit),
@@ -54,6 +65,8 @@ class FSignupForm extends StatelessWidget {
 
           ///Email
           TextFormField(
+            validator: (value) => FValidator.validateEmail(value),
+            controller: controller.email,
             decoration: const InputDecoration(
               label: Text(FTexts.email),
               prefixIcon: Icon(Iconsax.direct),
@@ -63,6 +76,8 @@ class FSignupForm extends StatelessWidget {
 
           ///Phone Number
           TextFormField(
+            validator: (value) => FValidator.validatePhoneNumber(value),
+            controller: controller.phoneNumber,
             decoration: const InputDecoration(
               label: Text(FTexts.phoneNo),
               prefixIcon: Icon(Iconsax.call),
@@ -72,9 +87,11 @@ class FSignupForm extends StatelessWidget {
 
           ///password
           TextFormField(
+            controller: controller.password,
+            validator: (value) => FValidator.validatePassword(value),
             obscureText: true,
             decoration: const InputDecoration(
-              labelText: FTexts.phoneNo,
+              labelText: FTexts.password,
               prefixIcon: Icon(Iconsax.password_check),
               suffixIcon: Icon(Iconsax.eye_slash),
             ),
@@ -90,7 +107,8 @@ class FSignupForm extends StatelessWidget {
           SizedBox(
             width: double.infinity,
             child: ElevatedButton(
-              onPressed: () => Get.to(() => const VerifyEmailScreen()),
+
+              onPressed: () => controller.signup(),
               child: const Text(FTexts.createAccount),
             ),
           ),
