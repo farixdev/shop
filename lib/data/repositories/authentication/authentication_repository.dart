f68f9a949dae7a137c79f792e3ee61558ae260a8
farixdev <onlyfarix@gmail.com>
2026-04-26 18:55:18 +0500

    feat: implement AuthenticationRepository for user session management and screen redirection

---
+++ lib/data/repositories/authentication/authentication_repository.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:shop/features/authentication/screens/login/login.dart';
+import 'package:shop/features/authentication/screens/onboarding/onboarding.dart';
+
+class AuthenticationRepository extends GetxController {
+  static AuthenticationRepository get instance => Get.find();
+
+  //Variable
+  final deviceStorage = GetStorage();
+
+  //Called from main.dart on app launch
+  @override
+  void onReady() {
+    FlutterNativeSplash.remove();
+    screenRedirect();
+  }
+
+  //Function to show relevant screen
+  screenRedirect() async {
+
+    if(kDebugMode)
+    {
+      print("======== get storage  Auth Repository =========");
+      print(deviceStorage.read('isFirstTime'));
+    }
+    //Local Storage
+    deviceStorage.writeIfNull('isFirstTime', true);
+    deviceStorage.read('isFirstTime') != true  ? Get.offAll(()=> LoginScreen()) : Get.offAll(OnBoardingScreen());
+  }
+}
