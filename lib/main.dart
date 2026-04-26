a0c38be2baa0955eacd2c0981a50710782a80cb1
farixdev <onlyfarix@gmail.com>
2026-04-26 18:54:53 +0500

    feat: refactor main function to include Firebase initialization and local storage setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get/get.dart';
+import 'package:get/route_manager.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:shop/data/repositories/authentication/authentication_repository.dart';
 import 'app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  
+Future<void> main() async {
+  //Widget binding
+  final WidgetsBinding widgetBinding =
+      WidgetsFlutterBinding.ensureInitialized();
+
+  //Add Local Storage
+  await GetStorage.init();
+
+  //Await Native Splash
+  FlutterNativeSplash.preserve(widgetsBinding: widgetBinding);
+
+  //initialize firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).then((FirebaseApp value) => Get.put(AuthenticationRepository()));
+
   runApp(const App());
 }
-
