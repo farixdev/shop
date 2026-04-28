31cddcde125bef9820a469d440a48d8e55346a88
farixdev <onlyfarix@gmail.com>
2026-04-28 11:45:36 +0500

    added the user controller to handel the storing of user data seperately rather the in auth repository

---
+++ lib/features/personalization/controllers/user_controller.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+import 'package:shop/data/repositories/user/user_repositry.dart';
+import 'package:shop/features/personalization/models/user_model.dart';
+import 'package:shop/utils/popups/loaders.dart';
+
+class UserController extends GetxController {
+  static UserController get to => Get.find();
+
+  final userRepositry = Get.put(UserRepositry());
+
+  //save user record from and Registration provider
+  Future<void> saveUserRecord(UserCredential? userCredentials) async {
+    try {
+      if (userCredentials != null) {
+        //Convert name to first and last name
+        final nameParts = UserModel.nameParts(
+          userCredentials.user!.displayName ?? '',
+        );
+        final username = UserModel.generateUsername(
+          userCredentials.user!.displayName ?? '',
+        );
+
+        //map data
+        final user = UserModel(
+          id: userCredentials.user!.uid,
+          firstName: nameParts[0],
+          lastName: nameParts.length > 1 ? nameParts.sublist(1).join(' ') : '',
+          username: username,
+          email: userCredentials.user!.email ?? '',
+          phoneNumber: userCredentials.user!.phoneNumber ?? '',
+          ProfilePicture: userCredentials.user!.photoURL ?? '',
+        );
+
+        //save user data
+        await userRepositry.saveUserRecord(user);
+      }
+    } catch (e) {
+      FLoaders.warningSnackBar(
+        title: 'data not saved',
+        message: 'Something went wrong. Please Try Again',
+      );
+    }
+  }
+}
