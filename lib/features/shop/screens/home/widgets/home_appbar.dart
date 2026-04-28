0409fcae881e650a01846a27749c96635fad4819
farixdev <onlyfarix@gmail.com>
2026-04-28 21:59:53 +0500

    added the controller instance of usercontroller

--- lib/features/shop/screens/home/widgets/home_appbar.dart
+++ lib/features/shop/screens/home/widgets/home_appbar.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 import 'package:shop/common/widgets/appbar/appbar.dart';
 
-
 import 'package:shop/common/widgets/products/cart/cart_menu_icon.dart';
+import 'package:shop/features/personalization/controllers/user_controller.dart';
 import 'package:shop/utils/constants/colors.dart';
 import 'package:shop/utils/constants/text_strings.dart';
 
-
 class FHomeAppBar extends StatelessWidget {
-  const FHomeAppBar({
-    super.key,
-  });
+  const FHomeAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(UserController());
     return FAppBar(
       title: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -27,14 +27,13 @@ class FHomeAppBar extends StatelessWidget {
           ),
           Text(
             FTexts.homeAppBarSubTitle,
-            style: Theme.of(context).textTheme.headlineSmall!
-                .apply(color: FColors.white),
+            style: Theme.of(
+              context,
+            ).textTheme.headlineSmall!.apply(color: FColors.white),
           ),
         ],
       ),
-      actions: [FCartCounterIcon(onPressed: () {  }, iconColor: FColors.white,)],
+      actions: [FCartCounterIcon(onPressed: () {}, iconColor: FColors.white)],
     );
   }
 }
-
-
