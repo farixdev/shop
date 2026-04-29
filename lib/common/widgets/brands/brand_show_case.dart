c054a85c74f3284ba174cdb76e0312b46c858bc8
farixdev <onlyfarix@gmail.com>
2026-04-30 01:19:43 +0500

    feat: implement comprehensive admin panel with management interfaces for products, categories, brands, orders, and banners

--- lib/common/widgets/brands/brand_show_case.dart
+++ lib/common/widgets/brands/brand_show_case.dart
@@ -1,6 +1,6 @@
 
 import 'package:flutter/material.dart';
-
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:shop/common/widgets/brands/brand_card.dart';
 import 'package:shop/common/widgets/custom_shapes/containers/rounded_container.dart';
 
@@ -13,10 +13,15 @@ import 'package:shop/utils/helpers/helper_functions.dart';
 
 
 
+import 'package:shop/features/shop/models/brand_model.dart';
+
 class FBrandShowcase extends StatelessWidget {
-  const FBrandShowcase({super.key, required this.images});
+  const FBrandShowcase({super.key, required this.images, required this.brand, this.onTap});
 
   final List<String> images;
+  final BrandModel brand;
+  final void Function()? onTap;
+
   @override
   Widget build(BuildContext context) {
     
@@ -29,10 +34,10 @@ class FBrandShowcase extends StatelessWidget {
       child: Column(
         children: [
           //Brand with product Count
-          const FBrandCard(showBorder: false , productCount: '250' ,),
+          FBrandCard(showBorder: false, brand: brand, onTap: onTap),
           const SizedBox(height: FSizes.defaultBtwItems),
 
-          //Brand top 3 profuct images
+          //Brand top 3 product images
           Row(
             children: images.map((image) => brandTopProductImageWidget(image, context)).toList()
           ),
@@ -49,11 +54,17 @@ class FBrandShowcase extends StatelessWidget {
         padding: const EdgeInsets.all(FSizes.md),
         margin: const EdgeInsets.only(right: FSizes.sm),
         backgroundColor: dark ? FColors.darkergrey : FColors.light,
-        child:  Image(
+        child: CachedNetworkImage(
+          imageUrl: image,
           fit: BoxFit.contain,
-          image: AssetImage(image),
+          progressIndicatorBuilder: (context, url, progress) => const Center(
+            child: CircularProgressIndicator(),
+          ),
+          errorWidget: (context, url, error) => const Icon(Icons.error),
         ),
       ),
     );
   }
 }
+
+
