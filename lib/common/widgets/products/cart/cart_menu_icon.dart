4606c69c490454a12cc2509b289ef60a23d0f276
farixdev <onlyfarix@gmail.com>
2026-04-30 01:19:56 +0500

    feat: implement core e-commerce shop features including product management, cart, favorites, and admin dashboard

--- lib/common/widgets/products/cart/cart_menu_icon.dart
+++ lib/common/widgets/products/cart/cart_menu_icon.dart
@@ -7,6 +7,9 @@ import 'package:shop/features/shop/screens/cart/cart.dart';
 import 'package:shop/utils/constants/colors.dart';
 import 'package:shop/utils/helpers/helper_functions.dart';
 
+import 'package:get/get.dart';
+import 'package:shop/features/shop/controllers/cart_controller.dart';
+
 class FCartCounterIcon extends StatelessWidget {
   const FCartCounterIcon({super.key, required this.onPressed, this.iconColor});
 
@@ -15,11 +18,12 @@ class FCartCounterIcon extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(CartController());
     final dark = FHelperFunctions.isDarkMode(context);
     return Stack(
       children: [
         IconButton(
-          onPressed: ()=> Get.to(() => CartScreen()),
+          onPressed: () => Get.to(() => const CartScreen()),
           icon: Icon(
             Iconsax.shopping_bag,
             color: iconColor ?? (dark ? FColors.white : FColors.dark),
@@ -27,20 +31,21 @@ class FCartCounterIcon extends StatelessWidget {
         ),
         Positioned(
           right: 0,
-
           child: Container(
             width: 18,
             height: 18,
             decoration: BoxDecoration(
-              color: FColors.black,
+              color: FColors.black.withOpacity(0.5),
               borderRadius: BorderRadius.circular(100),
             ),
             child: Center(
-              child: Text(
-                '2',
-                style: Theme.of(context).textTheme.labelLarge!.apply(
-                  color: FColors.white,
-                  fontSizeFactor: 0.8,
+              child: Obx(
+                () => Text(
+                  controller.noOfCartItems.value.toString(),
+                  style: Theme.of(context).textTheme.labelLarge!.apply(
+                    color: FColors.white,
+                    fontSizeFactor: 0.8,
+                  ),
                 ),
               ),
             ),
