bcc1c7821cf13f7a2a66323796d83ccc670fb94c
farixdev <onlyfarix@gmail.com>
2026-04-30 01:20:01 +0500

    feat: implement comprehensive shop features including admin management, product catalog, cart, user personalization, and repository services.

--- lib/common/widgets/products/product_cards/product_card_horizontal.dart
+++ lib/common/widgets/products/product_cards/product_card_horizontal.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
 import 'package:shop/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:shop/common/widgets/icons/f-circular-icon.dart';
@@ -7,16 +7,21 @@ import 'package:shop/common/widgets/images/rounded_image.dart';
 import 'package:shop/common/widgets/texts/brand_title_with_verified_icon.dart';
 import 'package:shop/common/widgets/texts/product_price_text.dart';
 import 'package:shop/common/widgets/texts/product_title_text.dart';
+import 'package:shop/features/shop/models/product_model.dart';
 import 'package:shop/utils/constants/colors.dart';
-import 'package:shop/utils/constants/image_strings.dart';
 import 'package:shop/utils/constants/sizes.dart';
 import 'package:shop/utils/helpers/helper_functions.dart';
+import 'package:shop/features/shop/controllers/favorites_controller.dart';
+import 'package:shop/features/shop/controllers/cart_controller.dart';
 
 class FProductCardHorizontal extends StatelessWidget {
-  const FProductCardHorizontal({super.key});
+  const FProductCardHorizontal({super.key, required this.product});
+
+  final ProductModel product;
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(FavoritesController());
     final dark = FHelperFunctions.isDarkMode(context);
     return Container(
       width: 310,
@@ -39,8 +44,9 @@ class FProductCardHorizontal extends StatelessWidget {
                   width: 120,
                   height: 120,
                   child: FRoundedImage(
-                    imageUrl: FImages.tshirt1,
+                    imageUrl: product.thumbnail,
                     applyImageRadius: true,
+                    isNetworkImage: true,
                   ),
                 ),
 
@@ -49,7 +55,6 @@ class FProductCardHorizontal extends StatelessWidget {
                   top: 12,
                   child: FRoundedContainer(
                     radius: FSizes.sm,
-                    // ignore: deprecated_member_use
                     backgroundColor: FColors.secondary.withOpacity(0.8),
                     padding: const EdgeInsets.symmetric(
                       horizontal: FSizes.sm,
@@ -68,7 +73,11 @@ class FProductCardHorizontal extends StatelessWidget {
                 Positioned(
                   top: 0,
                   right: 0,
-                  child: FCircularIcon(icon: Iconsax.heart, color: Colors.red),
+                  child: Obx(() => FCircularIcon(
+                    icon: controller.isFavourite(product.id) ? Iconsax.heart : Iconsax.heart_copy,
+                    color: controller.isFavourite(product.id) ? Colors.red : null,
+                    onPressed: () => controller.toggleFavoriteProduct(product.id),
+                  )),
                 ),
               ],
             ),
@@ -85,41 +94,44 @@ class FProductCardHorizontal extends StatelessWidget {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       FProductTitleText(
-                        title: 'T-Shirt Black Printed',
+                        title: product.title,
                         smallSize: true,
                       ),
                       const SizedBox(height: FSizes.defaultBtwItems / 2),
-                      FBrandTitleTextWithVerifiedIcon(title: 'Nike'),
+                      FBrandTitleTextWithVerifiedIcon(title: product.brand),
                     ],
                   ),
-                  Spacer(),
+                  const Spacer(),
 
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       //Pricing
-                      Flexible(child: FProductPriceText(price: '123'),),
+                      Flexible(child: FProductPriceText(price: product.price.toString()),),
+
+
 
                       ///Add to cart
-                      Container(
-                        decoration: BoxDecoration(
-                          color: FColors.dark,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(FSizes.cardRadiusMd),
-                            bottomRight: Radius.circular(
-                              FSizes.productImageRadius,
+                      GestureDetector(
+                        onTap: () => Get.put(CartController()).addProductToCart(product),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: FColors.dark,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(FSizes.cardRadiusMd),
+                              bottomRight: Radius.circular(FSizes.productImageRadius),
                             ),
                           ),
-                        ),
-
-                        child: SizedBox(
-                          width: FSizes.iconLg * 1.2,
-                          height: FSizes.iconLg * 1.2,
-
-                          child: Center(
-                            child: Icon(
-                              Iconsax.add_copy,
-                              color: FColors.white,
+                          child: SizedBox(
+                            width: FSizes.iconLg * 1.2,
+                            height: FSizes.iconLg * 1.2,
+                            child: Center(
+                              child: Obx(() {
+                                final count = Get.put(CartController()).getProductQuantityInCart(product.id);
+                                return count > 0 
+                                  ? Text('$count', style: Theme.of(context).textTheme.bodyLarge!.apply(color: FColors.white))
+                                  : const Icon(Iconsax.add_copy, color: FColors.white);
+                              }),
                             ),
                           ),
                         ),
